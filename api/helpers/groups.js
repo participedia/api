0d67cd2c780079aeba2215208f98af16732f4059
David Ascher <david.ascher@gmail.com>
2017-01-30 17:11:40 -0800

    removing console logs

--- api/helpers/groups.js
+++ api/helpers/groups.js
@@ -2,23 +2,18 @@ var jwt = require('./jwt')()
 
 function userHas (req, groupName, errCB, okCB) {
   try {
-    console.log('calling jwt')
     jwt(req, req.res,  function (err) {
       if (err) {
         errCB && errCB(err)
       } else {
         console.log("unhandled error in jwt.userHas", err)
       }
-      console.log("err", err)
       user = req.user
       if (user && user.app_metadata && user.app_metadata.authorization &&
           user.app_metadata.authorization.groups) {
-          console.log('all good');
         if (user.app_metadata.authorization.groups.indexOf(groupName) === -1) {
-          console.log('no group');
           errCB && errCB(err)
         } else {
-          console.log('OK');
           okCB && okCB()
         }
       }
