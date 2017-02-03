ed1014800e9a3eaa9ab0ef44f925a02e25025e93
David Ascher <david.ascher@gmail.com>
2017-02-03 09:31:11 -0800

    adding logging

--- api/helpers/db.js
+++ api/helpers/db.js
@@ -6,8 +6,14 @@ var promise = require('bluebird');
 var pgp = require('pg-promise')(options);
 var connectionString = process.env.DATABASE_URL;
 var parse = require('pg-connection-string').parse;
-var config = parse(connectionString)
-config['ssl'] = true
-var db = pgp(config);
+var config;
+
+try {
+  config = parse(connectionString)
+  config['ssl'] = true
+  var db = pgp(config);
+} catch (e) {
+  console.log("# Error parsing DATABASE_URL environment variable")
+}
 
 module.exports = exports = db
