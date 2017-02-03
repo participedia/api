d67e75cee76eaa0f6bba43fc5b6f30db9e6db6c0
David Ascher <david.ascher@gmail.com>
2017-02-03 11:32:45 -0800

    clean up commented out stuff

--- app.js
+++ app.js
@@ -10,19 +10,6 @@ var jwt = require('./api/helpers/jwt')()
 var config = {
   appRoot: path.join(__dirname, '..'), // required config
   validateResponse: false
-  // swaggerSecurityHandlers: {
-  //   token: function (req, authOrSecDef, scopesOrApiKey, cb) {
-  //     jwt(req, req.res, function (err) {
-  //       if (err) { console.log('err:', err) }
-  //       if (req.user === undefined) {
-  //         return cb(new Error('access denied - user does not exist in auth0'))
-  //       } else {
-  //         // console.log(req.user); // Contains { iss: 'https://xxx.auth0.com/', sub: 'auth0|xxx', ... }
-  //         return cb(null)
-  //       }
-  //     })
-  //   }
-  // }
 }
 var express = require('express')
 var compression = require('compression')
