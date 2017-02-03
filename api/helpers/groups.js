464bca0b3f29ed7f2e237095a3a449ee3fef50fc
David Ascher <david.ascher@gmail.com>
2017-02-03 09:28:58 -0800

    tolerate no-user scenarios

--- api/helpers/groups.js
+++ api/helpers/groups.js
@@ -5,15 +5,17 @@ function userHas (req, groupName, errCB, okCB) {
       user = req.user
       if (user && user.app_metadata && user.app_metadata.authorization &&
           user.app_metadata.authorization.groups) {
-
         if (user.app_metadata.authorization.groups.indexOf(groupName) === -1) {
           errCB && errCB(err)
         } else {
           okCB && okCB()
         }
+      } else {
+        errCB("no user")
       }
   } catch (e) {
     console.log('exception in userHas', e)
+    errCB("exception in userHas")
   }
 }
 
