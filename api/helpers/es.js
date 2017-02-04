0d2227e0d36319360bf5eae2e17ce0ab1cf2f61b
David Ascher <david.ascher@gmail.com>
2017-02-04 12:20:35 -0800

    Configure ElasticSearch to use AWS config

--- api/helpers/es.js
+++ api/helpers/es.js
@@ -1,21 +1,22 @@
 var process = require('process')
 var elasticsearch = require('elasticsearch')
 require('dotenv').config({silent: process.env.NODE_ENV === 'production'})
+var AWS = require("aws-sdk")
+
+AWS.config.update({
+  profile: "pp",
+  region: "us-east-1"
+});
 
 var client = new elasticsearch.Client({
-  // host: 'search-ppsandbox-6atza6pvphfjp73xqg7yhyl5qi.us-east-1.es.amazonaws.com',
   host: 'search-pp-stage-37xn6cdq7tj7ehv5rjrgxgrjhq.us-east-1.es.amazonaws.com',
   connectionClass: require('http-aws-es'),
   amazonES: {
     region: 'us-east-1',
-    accessKey: process.env.AWS_ACCESS_KEY_ID,
-    secretKey: process.env.AWS_SECRET_ACCESS_KEY
+    credentials: AWS.config.credentials
   },
   // log: 'debug'
 })
 
-// var client = new elasticsearch.Client({
-//   hosts: 'localhost:32769',
-// })
 
 module.exports = exports = client
