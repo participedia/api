8049ab74a82641bdce67cd418b829988055cc7cd
dethe <dethe@livingcode.org>
2017-03-03 19:27:55 -0800

    add missing migration

---
+++ migrations/migration_002.sql
@@ -0,0 +1,34 @@
+CREATE TABLE organization__locations (
+    location_name TEXT,
+    address1 TEXT,
+    address2 TEXT,
+    city TEXT,
+    province TEXT,
+    country TEXT,
+    postal_code TEXT,
+    latitude TEXT, /* lat/long should probably be a PostGIS geography point in one column */
+    longitude TEXT,
+    organization_id INTEGER REFERENCES organizations(id)
+);
+
+CREATE TABLE case__locations (
+    location_name TEXT,
+    address1 TEXT,
+    address2 TEXT,
+    city TEXT,
+    province TEXT,
+    country TEXT,
+    postal_code TEXT,
+    latitude TEXT, /* lat/long should probably be a PostGIS geography point in one column */
+    longitude TEXT,
+    case_id INTEGER REFERENCES cases(id)
+);
+
+INSERT INTO organization__locations (location_name, address1, address2, city, province, country, postal_code, latitude, longitude, organization_id) SELECT geolocation.name, geolocation.address1, geolocation.address2, geolocation.city, geolocation.province, geolocation.country, geolocation.postal_code, geolocation.latitude, geolocation.longitude, organizations.id from geolocation, organizations where organizations.location = geolocation.id;
+
+INSERT INTO case__locations (location_name, address1, address2, city, province, country, postal_code, latitude, longitude, case_id) SELECT geolocation.name, geolocation.address1, geolocation.address2, geolocation.city, geolocation.province, geolocation.country, geolocation.postal_code, geolocation.latitude, geolocation.longitude, cases.id from geolocation, cases where cases.location = geolocation.id;
+
+ALTER TABLE organizations DROP COLUMN location;
+ALTER TABLE cases DROP COLUMN location;
+
+DROP TABLE geolocation;
