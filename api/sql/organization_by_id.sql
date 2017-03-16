5c23e6fd1c8171ebc6173459c1418acaef43e892
dethe <dethe@livingcode.org>
2017-03-16 15:21:33 -0700

    organizations working with front-end

--- api/sql/organization_by_id.sql
+++ api/sql/organization_by_id.sql
@@ -1,17 +1,30 @@
 SELECT
-    organizations.*,
+    organizations.id,
+    organizations.original_language,
+    organizations.executive_director,
+    organizations.issue,
+    organizations.post_date,
+    organizations.published,
+    organizations.sector,
+    organizations.updated_date,
+    to_json(organizations.location) AS location,
+    to_json(organizations.lead_image) AS lead_image,
+    to_json(COALESCE(organizations.other_images, '{}')) AS other_images,
+    to_json(COALESCE(organizations.files, '{}')) AS files,
+    to_json(COALESCE(organizations.videos, '{}')) AS videos,
+    to_json(COALESCE(organizations.tags, '{}')) AS tags,
     organization__localized_texts.*,
-    to_json(author_list.authors)
+    to_json(author_list.authors) AS authors
 FROM
     organizations,
     organization__localized_texts,
     (
         SELECT
-            array_agg(ROW(
+            array_agg(CAST(ROW(
                 organization__authors.user_id,
                 organization__authors.timestamp,
                 users.name
-            )) authors,
+            ) AS author )) authors,
             organization__authors.organization_id
         FROM
             organization__authors,
