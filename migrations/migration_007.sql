0241ba60bef99f1e1ba753c1806b05935029cb7a
David Ascher <david.ascher@gmail.com>
2017-03-28 21:42:29 -0700

    Fix case insert bug
    As per http://hcmc.uvic.ca/blogs/index.php?blog=22&p=8105&more=1&c=1&tb=1&pb=1

--- migrations/migration_007.sql
+++ migrations/migration_007.sql
@@ -1,3 +1,3 @@
-select setval('cases_id_seq', (select max(id) from cases));
-select setval('methods_id_seq', (select max(id) from methods));
-select setval('organizations_id_seq', (select max(id) from organizations));
+select setval('cases_id_seq', (select max(id) from cases)+1);
+select setval('methods_id_seq', (select max(id) from methods)+1);
+select setval('organizations_id_seq', (select max(id) from organizations)+1);
