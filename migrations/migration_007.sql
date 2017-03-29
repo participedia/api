8f3e018e7bc12e3fff2ea7e75c90bc222de93790
dethe <dethe@livingcode.org>
2017-03-28 21:44:15 -0700

    Update the code to update id incrementing

--- migrations/migration_007.sql
+++ migrations/migration_007.sql
@@ -1,3 +1,4 @@
-select setval('cases_id_seq', (select max(id) from cases));
-select setval('methods_id_seq', (select max(id) from methods));
-select setval('organizations_id_seq', (select max(id) from organizations));
+select setval('cases_id_seq', (select max(id) from cases) +1 );
+select setval('methods_id_seq', (select max(id) from methods) +1);
+select setval('organizations_id_seq', (select max(id) from organizations) +1);
+select setval('users_id_seq', (select max(id) from users) + 1);
