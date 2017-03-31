fe4b42ec5a61fb95deee9269599ab88af87459ab
David Ascher <david.ascher@gmail.com>
2017-03-31 14:59:33 -0700

    forgot one file in prettification

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-'use strict'
-var process = require('process')
-require('dotenv').config({silent: process.env.NODE_ENV === 'production'})
-var http = require('http')
-var app = require('./app')
+"use strict";
+let process = require("process");
+require("dotenv").config({ silent: process.env.NODE_ENV === "production" });
+let http = require("http");
+let app = require("./app");
 
-http.createServer(app).listen(app.get('port'), function () {
-  console.log('Express server listening on port ' + app.get('port'))
-})
+http.createServer(app).listen(app.get("port"), function() {
+  console.log("Express server listening on port " + app.get("port"));
+});
