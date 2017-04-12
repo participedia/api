80f5f6ace1ac3715988432cc46845948df780b57
David Ascher <david.ascher@gmail.com>
2017-04-12 16:34:59 -0700

    remove dumps

--- api/helpers/user.js
+++ api/helpers/user.js
@@ -19,7 +19,6 @@ async function ensureUser(req, res, next) {
     let user = req.user;
     let name = req.header("X-Auth0-Name");
     let auth0UserId = req.header("X-Auth0-UserId");
-    console.log("ENSURE USER", user);
     if (!user) {
       return res.status(401).json({
         message: "User must be logged in to perform this function"
@@ -33,18 +32,14 @@ async function ensureUser(req, res, next) {
     if (user.user_id) {
       // all is well, carry on, but make sure user_id is a number
       user.user_id = as.number(user.user_id);
-      console.log("ALL IS WELL");
       return next();
     }
-    console.log("GETTNG USER BY EMAIL", user.email);
     userObj = await db.oneOrNone(sql("../sql/user_by_email.sql"), {
       userEmail: user.email
     });
-    console.log("GOT USER", userObj);
     if (userObj) {
       req.user.user_id = userObj.id;
     } else {
-      console.log("INSERTING USER");
       req.user.id = await db.one(sql("../sql/create_user_id.sql"), {
         userEmail: user.email,
         userName: name,
