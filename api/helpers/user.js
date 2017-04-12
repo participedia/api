aa31e435a5d65b3404b2163a009cd4c59e1fa6ac
David Ascher <david.ascher@gmail.com>
2017-04-12 16:34:34 -0700

    fix user adding; add user's join date (as defined by auth0) on user creation;

--- api/helpers/user.js
+++ api/helpers/user.js
@@ -19,6 +19,7 @@ async function ensureUser(req, res, next) {
     let user = req.user;
     let name = req.header("X-Auth0-Name");
     let auth0UserId = req.header("X-Auth0-UserId");
+    console.log("ENSURE USER", user);
     if (!user) {
       return res.status(401).json({
         message: "User must be logged in to perform this function"
@@ -32,17 +33,22 @@ async function ensureUser(req, res, next) {
     if (user.user_id) {
       // all is well, carry on, but make sure user_id is a number
       user.user_id = as.number(user.user_id);
+      console.log("ALL IS WELL");
       return next();
     }
-    user = await db.one(sql("../sql/user_by_email.sql"), {
+    console.log("GETTNG USER BY EMAIL", user.email);
+    userObj = await db.oneOrNone(sql("../sql/user_by_email.sql"), {
       userEmail: user.email
     });
-    if (user) {
-      req.user.user_id = user.id;
+    console.log("GOT USER", userObj);
+    if (userObj) {
+      req.user.user_id = userObj.id;
     } else {
+      console.log("INSERTING USER");
       req.user.id = await db.one(sql("../sql/create_user_id.sql"), {
         userEmail: user.email,
         userName: name,
+        joinDate: user.created_at,
         auth0UserId: auth0UserId
       });
     }
--- api/sql/create_user_id.sql
+++ api/sql/create_user_id.sql
@@ -3,13 +3,15 @@ INSERT into users (
   language,
   language_1,
   name,
-  auth0_user_id
+  auth0_user_id,
+  join_date
 )
 VALUES
   (
     ${userEmail},
     'en', 'en',
     ${userName},
-    ${auth0UserId}
+    ${auth0UserId},
+    ${joinDate}
   )
 RETURNING id as user_id;
