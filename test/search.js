2ee68c485b297d2b655782bbcbf59d06dff22f55
dethe <dethe@livingcode.org>
2017-05-02 14:39:48 -0700

    reshape faceted query results to match full text results

--- test/search.js
+++ test/search.js
@@ -63,9 +63,9 @@ describe("Search", () => {
         .send({})
         .end((err, res) => {
           res.should.have.status(200);
-          res.body.results.should.have.lengthOf(1);
-          res.body.results[0].type.should.equal("organization");
-          res.body.results[0].hits.should.have.lengthOf(17);
+          res.body.OK.should.equal(true);
+          res.body.results.should.have.lengthOf(17);
+          res.body.results.all.type.should.equal("organization");
           done();
         });
     });
@@ -101,9 +101,9 @@ describe("Search", () => {
         .send({})
         .end((err, res) => {
           res.should.have.status(200);
-          res.body.results.should.have.lengthOf(3);
-          res.body.results[0].type.should.equal("case");
-          res.body.results[0].hits.should.have.lengthOf(2);
+          res.body.OK.should.equal(true);
+          res.body.results.should.have.lengthOf(2);
+          // more testing
           done();
         });
     });
