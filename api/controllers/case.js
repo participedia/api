4708871dde2b9f514c0e5571ee5c73327951badd
David Ascher <david.ascher@gmail.com>
2017-06-17 20:31:57 -0700

    Don't get in a situation where you may send a 200 and a 500

--- api/controllers/case.js
+++ api/controllers/case.js
@@ -235,11 +235,15 @@ router.post("/new", async function postNewCase(req, res) {
       data: thing,
       object: newCase
     });
-    // Refresh search index
-    await db.none("REFRESH MATERIALIZED VIEW search_index_en;");
   } catch (error) {
     log.error("Exception in POST /case/new => %s", error);
     return res.status(500).json({ OK: false, error: error });
+  }
+  // Refresh search index
+  try {
+    await db.none("REFRESH MATERIALIZED VIEW search_index_en;");
+  } catch (error) {
+    log.error("Exception in POST /case/new => %s", error);
   }
 });
 
