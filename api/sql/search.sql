65d56f6fc3b884d3c10fdfe486b2adc5bc656f92
dethe <dethe@livingcode.org>
2018-04-28 17:14:00 -0700

    fix search order

--- api/sql/search.sql
+++ api/sql/search.sql
@@ -8,11 +8,12 @@ WITH all_selections AS (SELECT
   id,
   title,
   description,
-  substring(body for 500) AS body
+  substring(body for 500) AS body,
+  ts_rank_cd(search_index_${language:raw}.document, to_tsquery('english', ${query})) as rank
 FROM search_index_${language:raw}
 WHERE document @@ to_tsquery('english', ${query})
   ${filter:raw}
-ORDER BY ts_rank(search_index_${language:raw}.document, to_tsquery('english', ${query})) DESC
+ORDER BY rank DESC
 ),
 total_selections AS (
   SELECT count(all_selections.id) AS total
@@ -31,10 +32,11 @@ SELECT
   to_json(COALESCE(things.videos, '{}')) AS videos,
   things.updated_date,
   bookmarked(things.type, things.id, ${userId}),
-  total_selections.total
+  total_selections.total,
+  all_selections.rank
 FROM all_selections, total_selections, things
-WHERE all_selections.id = things.id AND
-  all_selections.id = things.id
+WHERE all_selections.id = things.id
+ORDER BY all_selections.rank DESC
 OFFSET ${offset}
 LIMIT ${limit}
 ;
