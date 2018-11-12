a12f955d9bd12ab088f0b60018e4256f4aa20ea3
dethe <dethe@livingcode.org>
2018-11-12 00:01:22 -0800

    updating partials

---
+++ migrations/migration_024.sql
@@ -0,0 +1,9 @@
+-- update keys to match view data
+
+-- functions to return all cases, all methods, all organizations
+
+-- add file label to static table
+
+-- Remove duplicates from case values
+
+-- Organize static value tags
