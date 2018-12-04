47de7eb05abce3756c982d15452dad32633bf64e
dethe <dethe@livingcode.org>
2018-12-03 19:15:32 -0800

    in progress

--- migrations/migration_024.sql
+++ migrations/migration_024.sql
@@ -1,3 +1,11 @@
+-- fix minor issues in get_case_by_id
+
+-- add get_raw_case_by_id
+
+-- add get_case_by_id_with_lists
+
+-- add get_raw_case_by_id_with_lists
+
 -- update keys to match view data
 
 -- functions to return all cases, all methods, all organizations
