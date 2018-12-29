857548e36d7f70a83f3a1ab5d8fb161106f1cb21
Alanna Scott <alanna.scott@gmail.com>
2018-12-28 20:09:49 -0800

    insert hidden fields for address component fields

--- public/js/edit-location.js
+++ public/js/edit-location.js
@@ -3,12 +3,31 @@
 
 const editLocation = {
   init() {
-    const inputEl = document.querySelector("[name=location_name]");
-    const autocomplete = new google.maps.places.Autocomplete(inputEl);
-    autocomplete.setFields(["address_components", "geometry", "formatted_address"]);
-    autocomplete.addListener("place_changed", function() {
-      // const place = autocomplete.getPlace();
-      // do something with place object if needed
+    this.inputEl = document.querySelector("[name=location_name]");
+    this.autocomplete = new google.maps.places.Autocomplete(this.inputEl);
+    this.autocomplete.setFields(["address_components", "geometry", "formatted_address"]);
+    this.autocomplete.addListener("place_changed", () => this.handlePlaceChange());
+  },
+
+  handlePlaceChange() {
+    const place = this.autocomplete.getPlace();
+
+    const addressComponents = {
+      city: place.address_components[0].long_name,
+      province: place.address_components[2].short_name,
+      country: place.address_components[3].long_name,
+      latitude: place.geometry.location.lat(),
+      longitude: place.geometry.location.lng(),
+    };
+
+    // insert hidden fields for the addressComponents values
+    const keys = Object.keys(addressComponents);
+    keys.forEach(key => {
+      const hiddenInputEl = document.createElement("input");
+      hiddenInputEl.setAttribute("type", "hidden");
+      hiddenInputEl.setAttribute("name", key);
+      hiddenInputEl.setAttribute("value", addressComponents[key]);
+      this.inputEl.insertAdjacentElement("afterend", hiddenInputEl);
     });
   }
 };
