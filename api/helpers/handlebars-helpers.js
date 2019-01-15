bb45eac7bc8b69a4d2f9899b8eb612fd2c8137d8
Alanna Scott <alanna.scott@gmail.com>
2019-01-14 18:53:59 -0800

    update helper for {key: 'key', value: 'value' } format

--- api/helpers/handlebars-helpers.js
+++ api/helpers/handlebars-helpers.js
@@ -19,15 +19,7 @@ function staticTextValue(staticText, name, type = null) {
     key = name;
   }
 
-  if (!staticText.labels) return key;
-
-  const labelObj = staticText.labels.find(item => item.key === key);
-
-  if (labelObj && labelObj.value) {
-    return labelObj.value;
-  } else {
-    return key;
-  }
+  return staticText.labels[key];
 }
 
 module.exports = {
