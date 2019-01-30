2cf00b14e4eb4998e2f75829f8ebe5b380b79df1
Alanna Scott <alanna.scott@gmail.com>
2019-01-29 16:05:04 -0800

    move card layout controls to tabs.js

--- public/js/home-search.js
+++ public/js/home-search.js
@@ -1,69 +1,10 @@
 import map from "./map.js";
 import tabs from "./tabs.js";
-import { updateUrlParams } from "./utils/utils.js";
 
 const homeSearch = {
   init() {
-    this.homeSearchEl = document.querySelector(".js-home-search");
-
-    if (!this.homeSearchEl) return;
-
     map.init();
-
     tabs.init();
-
-    this.initCardLayout();
-
-    this.initPagination();
-
-    this.initSortBy();
-
-  },
-
-  initSortBy() {
-    const sortByMenuEl = this.homeSearchEl.querySelector(".js-sort-by-menu");
-    sortByMenuEl.addEventListener("click", event => {
-      const link = event.target.closest("a");
-      if (link) {
-        event.preventDefault();
-        const sortBy = link.getAttribute("data-sortby");
-        updateUrlParams("sortby", sortBy);
-        window.location.href = window.location.href;
-      }
-    });
-  },
-
-  initPagination() {
-    const paginationNavEls = Array.prototype.slice.call(
-      this.homeSearchEl.querySelectorAll(".js-pagination-nav")
-    );
-
-    paginationNavEls.forEach(el => {
-      el.addEventListener("click", event => {
-        event.preventDefault();
-        const link = event.target.closest("a");
-        if (link) {
-          updateUrlParams("page", link.getAttribute("data-page-num"));
-          window.location.href = window.location.href;
-        }
-      });
-    });
-  },
-
-  initCardLayout() {
-    const toggleLayoutBtnsEl = this.homeSearchEl.querySelector(".js-card-layout-btns");
-
-    // event listeners for grid/list toggle buttons
-    toggleLayoutBtnsEl.addEventListener("click", event => {
-      const btnEl = event.target.closest("button");
-
-      if (btnEl) {
-        const type = btnEl.getAttribute("data-type");
-        if (!type) return;
-        updateUrlParams("layout", type);
-        this.homeSearchEl.setAttribute("data-layout", type);
-      }
-    });
   },
 }
 
--- public/js/tabs.js
+++ public/js/tabs.js
@@ -5,17 +5,28 @@ const tabs = {
     this.tabInputEls = Array.prototype.slice.call(
       document.querySelectorAll(".js-tab-container input[name='tabs']")
     );
+    this.viewEl = document.querySelector("[data-card-layout]");
 
     if (this.tabInputEls.length === 0) return;
 
+    // tabs ui
+    this.initDesktopTabNav();
+    this.initMobileTabNav();
+
+    if (!this.viewEl) return;
+    // cards ui
+    this.initCardLayout();
+    this.initPagination();
+    this.initSortBy();
+  },
+
+  initDesktopTabNav() {
     // update url param to indicate current tab
     this.tabInputEls.forEach(el => {
       el.addEventListener("click", event => {
         updateUrlParams("tab", event.target.id);
       });
     });
-
-    this.initMobileTabNav();
   },
 
   initMobileTabNav() {
@@ -36,6 +47,52 @@ const tabs = {
       this.tabInputEls.forEach(el => el.checked = el.id === newTabId);
       // update url
       updateUrlParams("tab", newTabId);
+    });
+  },
+
+  initSortBy() {
+    const sortByMenuEl = this.viewEl.querySelector(".js-sort-by-menu");
+    sortByMenuEl.addEventListener("click", event => {
+      const link = event.target.closest("a");
+      if (link) {
+        event.preventDefault();
+        const sortBy = link.getAttribute("data-sortby");
+        updateUrlParams("sortby", sortBy);
+        window.location.href = window.location.href;
+      }
+    });
+  },
+
+  initPagination() {
+    const paginationNavEls = Array.prototype.slice.call(
+      this.viewEl.querySelectorAll(".js-pagination-nav")
+    );
+
+    paginationNavEls.forEach(el => {
+      el.addEventListener("click", event => {
+        event.preventDefault();
+        const link = event.target.closest("a");
+        if (link) {
+          updateUrlParams("page", link.getAttribute("data-page-num"));
+          window.location.href = window.location.href;
+        }
+      });
+    });
+  },
+
+  initCardLayout() {
+    const toggleLayoutBtnsEl = this.viewEl.querySelector(".js-card-layout-btns");
+
+    // event listeners for grid/list toggle buttons
+    toggleLayoutBtnsEl.addEventListener("click", event => {
+      const btnEl = event.target.closest("button");
+
+      if (btnEl) {
+        const type = btnEl.getAttribute("data-type");
+        if (!type) return;
+        updateUrlParams("layout", type);
+        this.viewEl.setAttribute("data-card-layout", type);
+      }
     });
   },
 };
