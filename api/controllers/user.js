14d75da7e72b0859feeae71cf2e4f9d8cd2ffc1c
Alanna Scott <alanna.scott@gmail.com>
2019-01-31 23:05:21 -0800

    add static keys and replace in user-edit template

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -138,25 +138,51 @@ async function getUserById(userId, req, res, view="view") {
       },
     };
 
-    // static text
-    const languageOptions = [{ key: "en", value: "English" }, { key: "fr", value: "French" }];
-    const staticTxt = {
-      languageOptions: languageOptions,
-    };
-
     // only pass down the neccessary keys for the edit view
     const userEditKeys = ["id", "hidden", "name", "email", "location", "language", "login", "picture_url", "bio", "isadmin", "join_date"]
     const userEditJSON = {};
     userEditKeys.forEach(key => userEditJSON[key] = userJSON[key]);
 
+    // static keys needed to user-edit
+    const languageOptions = [{ key: "en", value: "English" }, { key: "fr", value: "French" }];
+    const userEditstaticTxt = {
+      languageOptions: languageOptions,
+      public_profile: "Public Profile",
+      change: "Change",
+      member_since: "Member Since",
+      name_label: "Name",
+      name_instructional: "name_instructional",
+      name_placeholder: "name placeholder",
+      location_label: "Location",
+      location_instructional: "location_instructional",
+      location_placeholder: "location placeholder",
+      bio_label: "Bio",
+      bio_instructional: "bio_instructional",
+      bio_placeholder: "bio placeholder",
+      private_settings: "Private Settings",
+      email_label: "Email",
+      email_instructional: "email_instructional",
+      email_placeholder: "email_placeholder",
+      password_label: "Password",
+      password_instructional: "password_instructional",
+      current_password_placeholder: "current_password_placeholder",
+      new_password_placeholder: "new_password_placeholder",
+      confirm_new_password_placeholder: "confirm_new_password_placeholder",
+      language_label: "language_label",
+      language_instructional: "language_instructional",
+      language_placeholder: "language_placeholder",
+      publish: "Publish",
+    }
+
+
     // depending on view, return correct template
     if (view === "edit") {
       return res.status(200).render("user-edit", {
         user: userEditJSON,
-        static: staticTxt,
+        static: userEditstaticTxt,
       });
     } else {
-      return res.status(200).render("user-view", { user: userJSON, static: staticTxt });
+      return res.status(200).render("user-view", { user: userJSON, static: {} });
     }
 
   } catch (error) {
