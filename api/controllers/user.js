15da94b19ad529e7e164d13758e5770d12a83cea
Alanna Scott <alanna.scott@gmail.com>
2019-01-31 22:13:38 -0800

    update json format on server and update in templates

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -17,11 +17,148 @@ async function getUserById(userId, req, res, view="view") {
         .json({ OK: false, error: `User not found for user_id ${userId}` });
     }
 
+    const userJSON = {
+      "id": 417689,
+      "hidden": null,
+      "name": "Alanna Scott",
+      "email": "alanna.scott@gmail.com",
+      "location": "Victoria, BC, Canada",
+      "language": "en",
+      "login": true,
+      "join_date": "2018-11-20T21:51:09.734",
+      "picture_url": "https://s.gravatar.com/avatar/d87986e91d456dfd25bdd9545d7cb51f?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Fal.png",
+      "bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sagittis justo id fermentum auctor. Aliquam iaculis mauris quis tempor viverra. Etiam commodo porta erat, sit amet ullamcorper augue. Quisque quam felis, vehicula ut efficitur at, sodales eu lacus. Nam mattis mauris sit amet lectus semper hendrerit. Morbi laoreet turpis ac enim vulputate, at condimentum lorem fringilla. Praesent quis justo ipsum. Ut at odio et enim tempor aliquet et et felis. Mauris quis ligula quis sem auctor egestas. Nullam quis tellus leo.",
+      "isadmin": false,
+      "type": "user",
+      "articles": {
+        "totalResults": 3,
+        "totalPages": 1,
+        "results": [
+          {
+            "id": 5239,
+            "type": "case",
+            "featured": false,
+            "title": "Community Justice Boards in Pima County, Arizona",
+            "description": "",
+            "body": "<p>Problems and Purpose</p><p>In the early 1990s, juvenile crime rates in Arizona reached their peak with the population of detainees reaching a record level. In response, the juvenile court system as well as detention facilities were expanded to meet the growing need, and Community Justice Boards were created in 1998.<a href=\"#ref%201\" name=\"cite%201\" id=\"cite 1\">[1]</a> The program offers an alternative for first and second-time, non-violent, juvenile offenders who would otherwise be prosecute",
+            "location_name": "Pima County Attorney's Office",
+            "address1": "32 N. Stone Ave Legal Services Building",
+            "address2": "",
+            "city": "Tucson",
+            "province": "Arizona",
+            "postal_code": "85701",
+            "country": "United States",
+            "latitude": "0° 0' 0\" N",
+            "longitude": "0° 0' 0\" E",
+            "images": [
+              "/images/texture_4.svg"
+            ],
+            "videos": [
+              "https://youtu.be/TSpqAZW7lyc"
+            ],
+            "updated_date": "2018-03-06T06:14:46.000Z",
+            "bookmarked": false
+          },
+          {
+            "id": 5237,
+            "type": "method",
+            "featured": false,
+            "title": "Sustained Dialogue",
+            "description": "",
+            "body": "<p><em>Note: the following entry is a stub. Please help us complete it. </em></p><h3>Definition</h3><p>Sustained Dialogue is defined as a 'changemaking process' which</p><ol><li>\"Focuses on transforming relationships that cause problems, create conflict, and block change; and</li><li>Emphasizes the importance of effective change over time\" [1]</li></ol><h3>Problems and Purpose History Participant Recruitment and Selection Deliberation, Decisions, and Public Interaction</h3><p>Sustained Dialogue ",
+            "location_name": "",
+            "address1": "",
+            "address2": "",
+            "city": "",
+            "province": "",
+            "postal_code": "",
+            "country": "",
+            "latitude": "",
+            "longitude": "",
+            "images": [
+              "/images/texture_1.svg"
+            ],
+            "videos": [],
+            "updated_date": "2018-03-04T17:38:43.000Z",
+            "bookmarked": false
+          },
+          {
+            "id": 327,
+            "type": "organization",
+            "featured": false,
+            "title": "Sustained Dialogue Institute",
+            "description": "",
+            "body": "<p>A complete entry on this organization is available at <a href=\"https://participedia.net/en/organizations/international-institute-sustained-dialogue\">https://participedia.net/en/organizations/international-institute-sustai...</a></p>",
+            "location_name": "",
+            "address1": "",
+            "address2": "",
+            "city": "Washington",
+            "province": "District Of Columbia",
+            "postal_code": "",
+            "country": "United States",
+            "latitude": "0° 0' 0\" N",
+            "longitude": "0° 0' 0\" E",
+            "images": [
+              "/images/texture_1.svg"
+            ],
+            "videos": [],
+            "updated_date": "2018-03-04T17:14:26.000Z",
+            "bookmarked": true
+          },
+        ],
+      },
+      "bookmarks": {
+        "totalResults": 1,
+        "totalPages": 1,
+        "results": [
+          {
+            "id": 327,
+            "type": "organization",
+            "featured": false,
+            "title": "Sustained Dialogue Institute",
+            "description": "",
+            "body": "<p>A complete entry on this organization is available at <a href=\"https://participedia.net/en/organizations/international-institute-sustained-dialogue\">https://participedia.net/en/organizations/international-institute-sustai...</a></p>",
+            "location_name": "",
+            "address1": "",
+            "address2": "",
+            "city": "Washington",
+            "province": "District Of Columbia",
+            "postal_code": "",
+            "country": "United States",
+            "latitude": "0° 0' 0\" N",
+            "longitude": "0° 0' 0\" E",
+            "images": [
+              "/images/texture_1.svg"
+            ],
+            "videos": [],
+            "updated_date": "2018-03-04T17:14:26.000Z",
+            "bookmarked": true
+          },
+        ],
+      },
+    };
+
+    // static text
+    const languageOptions = [{ key: "en", value: "English" }, { key: "fr", value: "French" }];
+    const staticTxt = {
+      languageOptions: languageOptions,
+    };
+
+    // only pass down the neccessary keys for the edit view
+    const userEditKeys = ["id", "hidden", "name", "email", "location", "language", "login", "picture_url", "bio", "isadmin"]
+    const userEditJSON = {};
+    userEditKeys.forEach(key => {
+      userEditJSON[key] = userJSON[key];
+    });
+
     // depending on view, return correct template
     if (view === "edit") {
-      return res.status(200).render("user-edit", { data: result.user, static: {} });
+      return res.status(200).render("user-edit", {
+        user: userEditJSON,
+        static: staticTxt,
+      });
     } else {
-      return res.status(200).render("user-view", { data: result.user, static: {} });
+      return res.status(200).render("user-view", { user: userJSON, static: staticTxt });
     }
 
   } catch (error) {
