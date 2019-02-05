8fb3572342ac86b9ca7465a3962bd1ddfaba7c43
Alanna Scott <alanna.scott@gmail.com>
2019-02-04 22:30:56 -0800

    update cypress, generalize case edit form test

---
+++ cypress/integration/article_edit_spec.js
@@ -0,0 +1,31 @@
+const articleTypes = ['case', 'method', 'organization'];
+
+articleTypes.forEach(type => editFormTest(type));
+
+function editFormTest(type) {
+  describe(`${type} edit form`, () => {
+    it(`visits /${type}/{id}/edit`, () => {
+      cy.visit(`http://localhost:3001/${type}/2/edit`);
+
+      // it has a submit button
+      cy.get("[data-cy=submit]").should("be.visible");
+
+      //it has a floating publish button
+      cy.get("[data-cy=floating-action-button]").should("be.visible");
+
+      // it has a link to full version
+      const fullVerLinkEl = cy.get("[data-cy=full-version-link]");
+      fullVerLinkEl.should("be.visible");
+      fullVerLinkEl.click();
+      cy.url().should('include', '?full=1')
+
+      // it does not have a link to full version if it's currently full version
+      cy.get("[data-cy=full-version-link]").should("not.be.visible");
+
+      // it submits the form without errors
+      cy.get("[data-cy=submit]").click();
+      // todo: verify that form was submittied successfully
+    });
+  });
+}
+
