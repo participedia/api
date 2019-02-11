e6ec50e8841f259ae08b2c3a1a415a95f9e4f504
Alanna Scott <alanna.scott@gmail.com>
2019-02-11 14:50:04 -0800

    add space between terms for readability

--- static-text/research-static-text.js
+++ static-text/research-static-text.js
@@ -1,15 +1,27 @@
 module.exports = {
   "research.brazilian_data_set_link": "Dataset on Brazilian Participatory Budgeting: 1989 to 2012",
+
   "research.data_repository.header": "Data Repository",
+
   "research.data_repository.p1": "A great deal of research on democratic innovations is not publicly available. This includes large datasets, graduate and postgraduate dissertations, and practitioner evaluations and reports. We aim to find a home for such research in an accessible data repository.",
+
   "research.header": "Research",
+
   "research.methodology.header": "Methodology",
+
   "research.methodology.p1": "The strategy is simple: crowdsource data on democratic innovations from around the world and then aggregate this into a public database that continually updates with new contributions. All of Participedia’s content and data is open source.",
+
   "research.methodology.p2": "For an in-depth explanation of the history, aspirations, theory and analytical approach of Participedia, see [needs-link]The Participedia Project: An Introduction [/needs-link], by the two founders of Participedia, Archon Fung and Mark E. Warren.",
+
   "research.surveys.header": "Surveys",
+
   "research.surveys.p1": "Surveys are supplementary to Participedia’s current data and narrative descriptions and are intended to gain further insight on the outcomes and effects of cases.",
+
   "research.surveys.p2": "The participant survey will capture the experience of participants directly involved in a participatory process. It could be delivered by organizers at the location of a particular process, or participants could be asked to complete the survey after the event.",
+
   "research.surveys.p3": "The observer survey will capture views pertaining to the broader impact of a particular case. ‘Observers’ include practitioners, participants or researchers with particular knowledge of that case.",
+
   "research.surveys.p4": "Participedia's surveys are undergoing testing, and will be available for download soon.",
+
   "research.tagline": "Participedia is guided by the research question: What kinds of participatory processes work best, for what purposes, and under what conditions?",
 };
