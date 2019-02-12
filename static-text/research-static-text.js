f8491a49ad8d02ee286626579ea432afdf8b30ed
Alanna Scott <alanna.scott@gmail.com>
2019-02-11 18:37:26 -0800

    update research text

--- static-text/research-static-text.js
+++ static-text/research-static-text.js
@@ -1,11 +1,31 @@
 module.exports = {
-  "research.brazilian_data_set_link": "Dataset on Brazilian Participatory Budgeting: 1989 to 2012",
+  "research.data_repository.dataverse": "Participedia Dataverse Repository",
 
-  "research.data_repository.p1": "A great deal of research on democratic innovations is not publicly available. This includes large datasets, graduate and postgraduate dissertations, and practitioner evaluations and reports. We aim to find a home for such research in an accessible data repository.",
+  "research.data_repository.p1": "A great deal of research on democratic innovations and participatory processes that takes place around the world is not publicly available. These materials include datasets, codebooks, statistical software syntax or scripts, dissertations, conference papers, and practitioner evaluations and reports. The <a href=\"https://dataverse.harvard.edu/dataverse/Participedia\" target=\"_blank\">Participedia Dataverse</a> provides a home for these materials in an accessible and organized data repository, archives the materials for long-term preservation, and generates standardized descriptions and identifiers to ease discovery and citation of the materials.",
 
-  "research.methodology.p1": "The strategy is simple: crowdsource data on democratic innovations from around the world and then aggregate this into a public database that continually updates with new contributions. All of Participedia’s content and data is open source.",
+  "research.data_repository.p2": "Researchers and practitioners can contribute to the Participedia Dataverse by depositing their research materials free of charge. Features of the Participedia data repository include:",
 
-  "research.methodology.p2": "For an in-depth explanation of the history, aspirations, theory and analytical approach of Participedia, see [needs-link]The Participedia Project: An Introduction [/needs-link], by the two founders of Participedia, Archon Fung and Mark E. Warren.",
+  "research.data_repository.feature.1": "metrics on the number of downloads of each dataset;",
+
+  "research.data_repository.feature.2": "the ability to download datasets in multiple formats that are automatically generated;",
+
+  "research.data_repository.feature.3": "the ability to conduct data analysis right through the user interface without downloading data, since R is built into the user interface;",
+
+  "research.data_repository.feature.4": "long-term preservation of data;",
+
+  "research.data_repository.feature.5": "automatic assignment of digital object identifiers (DOIs) and other standards-compliant metadata to enable citation of datasets;",
+
+  "research.data_repository.feature.6": "indexing of datasets in major search engines to help users find the data;",
+
+  "research.data_repository.feature.7": "creation of digital fingerprints to enable authentication of datasets; and",
+
+  "research.data_repository.feature.8": "an open API that allows data and metadata to be used by other web services.",
+
+  "research.data_repository.questions": "Please direct any questions you may have about how to contribute to Participedia Dataverse to <a href=\"mailto:data@participedia.net\">data@participedia.net</a>",
+
+  "research.methodology.p1": "The strategy is simple: crowdsource information on democratic innovations from around the world and then aggregate this into a public database that continually updates with new contributions. All of Participedia’s content and data is open source.",
+
+  "research.methodology.p2": "For an in-depth explanation of the history, aspirations, theory, and analytical approach of Participedia, see '<a href=\"http://archonfung.net/docs/articles/2011/FungParticipedia201112.pdf\">The Participedia Project: An Introduction</a>', by the two founders of Participedia, Archon Fung and Mark E. Warren.",
 
   "research.surveys.p1": "Surveys are supplementary to Participedia’s current data and narrative descriptions and are intended to gain further insight on the outcomes and effects of cases.",
 
