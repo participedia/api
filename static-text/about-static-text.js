ea73ad3f3f0a571f9ed01b703bfa4b812cd30ead
Alanna Scott <alanna.scott@gmail.com>
2019-03-07 10:20:47 -0800

    update about text up to partners section

--- static-text/about-static-text.js
+++ static-text/about-static-text.js
@@ -7,29 +7,29 @@ module.exports = {
 
   "about.committees.p1": "Committees are comprised of project co-investigators and collaborators, each with one nominated chairperson who sits on the executive committee.",
 
-  "about.community.p1": "Participedia’s searchable database of democratic innovations is made up of three distinct content types including Cases, Methods and Organizations. Bolstering this knowledge base are added resources, including surveys, teaching tools and external data sets.",
+  "about.community.p1": "Participedia’s searchable database of participatory democracy is for anyone - from the armchair researcher to the social scientist. The platform was collaboratively designed by an international research partnership to connect and bolster their work on participatory democracy with publicly crowdsourced knowledge.",
 
-  "about.community.p2": "The initial vision for Participedia was developed by Archon Fung (Kennedy School of Government, Harvard University) and Mark E. Warren (Department of Political Science, University of British Columbia), and is guided by a set of standing committees.",
+  "about.community.p2": "Participedia was founded by Archon Fung (Kennedy School of Government, Harvard University) and Mark E. Warren (Department of Political Science, University of British Columbia), and is guided by a set of standing committees.",
 
   "about.content.p1": "Participedia offers a searchable database of content related to worldwide public participation.",
 
-  "about.contribute.p1": "Help improve the quality of this knowledge resource by editing existing content or publishing your own.",
+  "about.contribute.p1": "Help expand our database by editing existing content or publishing your own.",
 
   "about.design_and_tech.description": "The Design & Technology Committee is responsible for the design and maintenance of the Participedia website.",
 
-  "about.explore.p1": `Search, read, download and gain insight from our database of <a href="/cases">Cases</a>, <a href="/methods">Methods</a>, <a href="/organizations">Organizations</a>, surveys, teaching resources and external data sets.`,
+  "about.explore.p1": "Search, read, download, and learn from our database of cases, methods, organizations, surveys, teaching resources and external data sets.",
 
   "about.funders.p1": "Participedia is currently supported by the Social Sciences and Humanities Research Council of Canada (SSHRC) in the form of a five-year Partnership Grant beginning in April 2015. A two-year SSHRC Partnership Grant was originally provided in April 2011. In 2012, The Bertelsmann Foundation provided generous support for development of multi-language functionality.",
 
   "about.get_involved.p1": `Anyone can <a href="/login">join the Participedia community</a> and help crowdsource, catalogue and compare participatory political processes around the world. All content on Participedia is collaboratively produced and open-source under a <a href="http://creativecommons.org/licenses/by-nc-sa/3.0/">Creative Commons License</a>.`,
 
-  "about.members.p1": "Community members include policy analysts, democratic theorists, scholars, teachers, practitioners, activists, government officials, journalists and more. Anyone can join and help to build this growing resource by editing existing content or publishing new examples of public participation.",
+  "about.members.p1": "Participedia is open to anyone. Our members include people of all backgrounds, nationalities, and professions. From policy analysts to professors, activists to teachers, Participedia is a place for anyone interested in democratic participation. Our community is at the heart of our endeavour: by joining up, you too can help build this growing resource by editing existing content or publishing new examples of public participation.",
 
   "about.methods.p1": "Methods are the processes and procedures used to guide participatory politics and governance.",
 
   "about.organizations.p1": "Organizations are profiles of formal and informal groups that design, implement, or support innovations in participatory politics and governance.",
 
-  "about.partners.p1": "Participedia reflects partnerships with organizations, institutions and individuals across the globe who share an interest in participatory politics and governance. Participedia’s partners are co-signers to the current SSHRC Partnership Grant. Please contact us if you would like to discuss developing a partnership to support Participedia’s global mission.",
+  "about.partners.p1": "Participedia is built on a global partnership between organizations, institutions, and individuals who share an interest in participatory politics and governance. Our partners include co-applicants and collaborators under the SSHRC Partnership Grant. Please contact us if you would like to support the Participedia Project's global mission to expand the use and knowledge of democratic innovation by becoming a partner.",
 
   "about.rdc.description": "The Research Design Committee is responsible for setting the strategic direction of PPedia’s research agenda.",
 
@@ -39,7 +39,7 @@ module.exports = {
 
   "about.tagline": "A global community sharing knowledge and stories about public participation",
 
-  "about.teach.p1": "Use Participedia in the classroom as a tool to engage students and showcase their research.",
+  "about.teach.p1": "Use Participedia in the classroom to engage students and showcase their research.",
 
   "about.ttmc.decription": "The Teaching Training and Mentoring Committee is responsible for developing outputs and implement actions for the integration of Participedia materials into pedagogy, teaching, student involvement and the training of practitioners and public officials.",
 
