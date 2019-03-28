9b32885581c91767aad93651629fc9d4c747b20d
Alanna Scott <alanna.scott@gmail.com>
2019-03-28 09:23:38 -0700

    add german

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.use(
 app.use(errorhandler());
 
 i18n.expressBind(app, {
-  locales: ["en", "fr"],
+  locales: ["en", "fr", "de"],
   defaultLocale: "en",
   extension: ".json",
   cookieName: "locale",
