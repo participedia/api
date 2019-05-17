ea7beefe557de4fc738039f2f8836e9d015196cd
Alanna Scott <alanna.scott@gmail.com>
2019-05-16 17:18:58 -0700

    use sortable library for cross-browser sorting

--- public/js/edit-multi-select.js
+++ public/js/edit-multi-select.js
@@ -1,14 +1,17 @@
 import Modal from "a11y-dialog-component";
+import Sortable from "sortablejs";
+
+const toArray = (nodeList) => Array.prototype.slice.call(nodeList);
 
 const editMultiSelect = {
   init() {
-    this.selectLists = document.querySelectorAll(".js-edit-multi-select-list");
+    this.selectLists = toArray(document.querySelectorAll(".js-edit-multi-select-list"));
 
     if (this.selectLists.length < 1) return;
 
     this.bindRemoveItemClicks();
     this.bindOnSelectChange();
-    this.bindDragHandlers();
+    this.initSortableLists();
 
     this.modalId = "#modal-container";
     this.modal = new Modal(this.modalId, {
@@ -16,60 +19,28 @@ const editMultiSelect = {
     });
   },
 
-  bindDragHandlers() {
-    let sourceLi;
+  initSortableLists() {
     this.selectLists.forEach(el => {
-      el.addEventListener("dragstart", (e) => {
-        sourceLi = e.target;
-        e.dataTransfer.dropEffect = "move";
-      });
-
-      el.addEventListener("dragover", (e) => {
-        e.preventDefault();
-        if (e.target.nodeName === "LI") {
-          e.dataTransfer.dropEffect = "move";
-        }
-      });
-
-      el.addEventListener("drop", (e) => {
-        e.preventDefault();
-        const parentList = e.target.closest("ol");
-        const allLiEls = parentList.querySelectorAll("li");
-        const name = parentList.getAttribute("data-name");
-        const droppedLi = e.target.closest("li");
-        const newSourceLi = `<li draggable="true">${sourceLi.innerHTML}</li>`;
-
-        const getIndexOfLi = (node) => {
-          for (var i = 0; i < allLiEls.length; i++) {
-            if (node === allLiEls[i]) break;
-          }
-          return i;
-        };
-
-        if (droppedLi) {
-          const droppedLiIndex = getIndexOfLi(droppedLi);
-          const sourceLiIndex = getIndexOfLi(sourceLi);
-
-          if (droppedLiIndex < sourceLiIndex) {
-            droppedLi.insertAdjacentHTML("beforebegin", newSourceLi);
-          } else {
-            droppedLi.insertAdjacentHTML("afterend", newSourceLi);
-          }
-
-          sourceLi.remove();
-
-          // update the input field names to reflect new order
-          parentList.querySelectorAll("input").forEach((el, i) => {
-            el.name =`${name}[${i}]`;
-          });
-        }
-      });
+      if (el.getAttribute("data-draggable")) {
+        Sortable.create(el, {
+          swapThreshold: 1,
+          animation: 150,
+          draggable: "li",
+          onEnd: e => this.updateIndexes(e),
+        });
+      }
     });
   },
 
-  isRanked(name) {
-    const currentSelect = document.querySelector(`.js-edit-multi-select[name=${name}]`);
-    return !!currentSelect.getAttribute("data-ranked");
+  updateIndexes(e) {
+    e.preventDefault();
+    const name = e.target.getAttribute("data-name");
+    const parentList = document.querySelector(`.js-edit-multi-select-list[data-name=${name}]`);
+
+    // update the input field names to reflect new order
+    toArray(parentList.querySelectorAll("input")).forEach((el, i) => {
+      el.name =`${name}[${i}]`;
+    });
   },
 
   bindRemoveItemClicks() {
@@ -84,7 +55,7 @@ const editMultiSelect = {
   },
 
   bindOnSelectChange() {
-    const selectEls = document.querySelectorAll(".js-edit-multi-select");
+    const selectEls = toArray(document.querySelectorAll(".js-edit-multi-select"));
     selectEls.forEach((el) => {
       el.addEventListener("change", (e) => {
         this.onSelectChange(e);
@@ -95,7 +66,7 @@ const editMultiSelect = {
   onSelectChange(e) {
     const name = e.target.name;
     const selectedItem = e.target.selectedOptions[0];
-    const currentList = document.querySelector(`.js-edit-multi-select-list[data-name=${e.target.name}]`);
+    const currentList = document.querySelector(`.js-edit-multi-select-list[data-name=${name}]`);
     const currentSelect = document.querySelector(`.js-edit-multi-select[name=${name}]`);
     const maxItems = currentList.getAttribute("data-max");
     const numItems = currentList.querySelectorAll("li").length;
@@ -103,7 +74,7 @@ const editMultiSelect = {
 
     const isInList = () => {
       // converts a NodeList into an Array so we can use the find method
-      const arrayOfInputs = Array.prototype.slice.call(currentList.querySelectorAll("input"));
+      const arrayOfInputs = toArray(currentList.querySelectorAll("input"));
       return arrayOfInputs.find(el => el.value === selectedItem.value);
     };
 
@@ -132,14 +103,12 @@ const editMultiSelect = {
   listItemTemplate(name, selectedItem, index) {
     const key = selectedItem.value;
     const value = selectedItem.innerText.trim();
-    const ranked = this.isRanked(name);
     const template = document.querySelector("#js-edit-multi-select-list-item");
     const newList = document.createElement("ol");
     newList.innerHTML = template.innerHTML;
     // set new data in template
     const newItem = newList.querySelector("li");
     const newItemInput = newItem.querySelector("input[type=hidden]");
-    if (ranked) newItem.setAttribute("draggable", ranked);
     newItem.querySelector("span").innerText = value;
     newItemInput.value = key;
     newItemInput.name = `${name}[${index}][key]`;
@@ -150,6 +119,7 @@ const editMultiSelect = {
     const listItem = e.target.closest("li");
     const listContainer = listItem.closest("ol");
     listContainer.removeChild(listItem);
+    this.updateIndexes(e);
   },
 }
 
