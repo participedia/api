f8fc23f5a68dab27072ad7991d007485fb75e06c
Alanna Scott <alanna.scott@gmail.com>
2019-05-20 21:05:26 -0700

    make removing and adding work for both single and multi fields

--- public/js/edit-autocomplete.js
+++ public/js/edit-autocomplete.js
@@ -25,12 +25,19 @@ const editAutocomplete = {
     const buttonEl = e.target.closest("button");
     if (!buttonEl) return;
 
-    const currentEl = buttonEl.closest("li");
-    const name = currentEl.getAttribute("data-name");
-    // reset hidden field value
-    const hiddenEl = currentEl.querySelector("input");
-    currentEl.style.display = "none";
-    hiddenEl.value = null;
+    const currentLiEl = buttonEl.closest("li");
+    const name = currentLiEl.getAttribute("data-name");
+    const parentListEl = currentLiEl.parentNode;
+    const numItems = parentListEl.querySelectorAll("li").length;
+
+    // if we have more than 1 item, remove it
+    // if there is only 1 item left, clear the value
+    if (numItems > 1) {
+      parentListEl.removeChild(currentLiEl);
+    } else {
+      currentLiEl.querySelector("input").value = null;
+      currentLiEl.style.display = "none";
+    }
   },
 
   getOptions(el) {
@@ -47,36 +54,48 @@ const editAutocomplete = {
     return options;
   },
 
-  addSelectedItem(autocompleteEl, name, item) {
+  addSelectedItem(name, item) {
     const listToAppendToEl = document.querySelector(
       `.js-edit-autocomplete-list[data-name=${name}]`
     );
-    const template = document.getElementById(
-      "js-edit-autocomplete-selected-item-template"
-    );
-    const newListContainer = document.createElement("ol");
-    newListContainer.innerHTML = template.innerHTML;
-    const newItemEl = newListContainer.querySelector("li");
-    const hiddenEl = newItemEl.querySelector("input");
-
-    // update name on li
-    newItemEl.setAttribute("data-name", name);
-    // update name and index on hidden input
-
-    if (listToAppendToEl.getAttribute("data-multi")) {
-      const index = listToAppendToEl.querySelectorAll("li").length;
-      hiddenEl.name = `${name}[${index}]`;
+    const isMulti = listToAppendToEl.getAttribute("data-multi");
+    const numItems = listToAppendToEl.querySelectorAll("li").length;
+    // if it's a single item autocomplete
+    if (!isMulti && numItems > 0) {
+      // update the field
+      const currentLiEl = listToAppendToEl.querySelectorAll("li")[0];
+      // update selected label
+      currentLiEl.querySelector("span").innerText = item.label;
+      // update value on hidden input
+      currentLiEl.querySelector("input").value = item.value;
+      currentLiEl.style.display = "flex";
     } else {
-      hiddenEl.name = name;
+      // if it's a multi field autocomplete, or there was no value, insert new item
+      const template = document.getElementById(
+        "js-edit-autocomplete-selected-item-template"
+      );
+      const newListContainer = document.createElement("ol");
+      newListContainer.innerHTML = template.innerHTML;
+      const newItemEl = newListContainer.querySelector("li");
+      const hiddenEl = newItemEl.querySelector("input");
+
+      // update name on li
+      newItemEl.setAttribute("data-name", name);
+
+      // update name and index on hidden input
+      if (listToAppendToEl.getAttribute("data-multi")) {
+        const index = listToAppendToEl.querySelectorAll("li").length;
+        hiddenEl.name = `${name}[${index}]`;
+      } else {
+        hiddenEl.name = name;
+      }
+      // update selected label
+      newItemEl.querySelector("span").innerText = item.label;
+      // update value on hidden input
+      hiddenEl.value = item.value;
+      //append template li to list
+      listToAppendToEl.insertAdjacentElement("beforeend", newItemEl);
     }
-    // update selected label
-    newItemEl.querySelector("span").innerText = item.label;
-    // update value on hidden input
-    hiddenEl.value = item.value;
-    // clear autocomplete field
-    autocompleteEl.value = "";
-    //append template li to list
-    listToAppendToEl.append(newItemEl);
   },
 
   initAutocompleteField(autocompleteEl) {
@@ -91,7 +110,9 @@ const editAutocomplete = {
         update(suggestions);
       },
       onSelect: item => {
-        this.addSelectedItem(autocompleteEl, name, item);
+        this.addSelectedItem(name, item);
+        // clear autocomplete field
+        autocompleteEl.value = "";
       }
     });
   }
