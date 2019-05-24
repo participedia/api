8bd4ae46b81284ef7aa79c0d4499f2ffaf52d2af
Alanna Scott <alanna.scott@gmail.com>
2019-05-24 11:17:13 -0700

    move into fn

--- public/js/hamburger-menu.js
+++ public/js/hamburger-menu.js
@@ -1,18 +1,28 @@
 const hamburgerMenu = {
   init() {
     this.bindCloseAnywhere();
+    this.bindMenuClicks();
 
+    // if the menu is already open on load, toggle body class
+    if (location.hash === "#sidebar-menu") {
+      document.querySelector("body").classList.add("sidebar-menu-is-open");
+    }
+  },
+
+  bindMenuClicks() {
     document.querySelector(".js-menu-icon-container").addEventListener("click", e => {
       const menuTriggerEl = e.target.closest("a");
       if (menuTriggerEl) {
         this.handleMenuClick(e);
       }
     });
+  },
 
-    // if the menu is open on load, toggle body class
-    if (location.hash === "#sidebar-menu") {
-      document.querySelector("body").classList.add("sidebar-menu-is-open");
-    }
+  bindCloseAnywhere() {
+    const bgClickElement = document.querySelector('.js-sidebar-menu');
+    bgClickElement.addEventListener('click', () => {
+      location.href = '#';
+    });
   },
 
   handleMenuClick(e) {
@@ -24,13 +34,6 @@ const hamburgerMenu = {
     } else {
       document.querySelector("body").classList.remove("sidebar-menu-is-open");
     }
-  },
-
-  bindCloseAnywhere() {
-    const bgClickElement = document.querySelector('.js-sidebar-menu');
-    bgClickElement.addEventListener('click', () => {
-      location.href = '#';
-    });
   },
 };
 
