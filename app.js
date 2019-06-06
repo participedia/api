1799b41ffac2b9622d5a811a3b0bfdf658cd45e3
Alanna Scott <alanna.scott@gmail.com>
2019-06-05 17:29:49 -0700

    add french

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ app.use(cookieParser());
 app.use(errorhandler());
 
 i18n.configure({
-  locales: ["en"],
+  locales: ["en", "fr"],
   cookie: "locale",
   extension: ".js",
   directory: "./locales",
