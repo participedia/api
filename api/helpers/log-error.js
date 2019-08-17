46e0223144d765a397e8821ec3e77589920f3bef
Alanna Scott <alanna.scott@gmail.com>
2019-08-17 13:07:15 -0700

    remove new relic

--- api/helpers/log-error.js
+++ api/helpers/log-error.js
@@ -2,18 +2,11 @@ const Sentry = require("@sentry/node");
 
 const isProdOrStaging = process.env.NODE_ENV === "production" || process.env.NODE_ENV === "staging";
 
-let newrelic;
-
-if (isProdOrStaging) {
-  newrelic = require("newrelic");
-}
-
 function logError(error, params = {}) {
   if (isProdOrStaging) {
     if (typeof error === "string") {
       Sentry.captureMessage(error);
     } else {
-      newrelic.noticeError(error, params);
       Sentry.captureException(error);
     }
   }
