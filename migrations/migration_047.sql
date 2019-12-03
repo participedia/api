25aaed8222945557a1097eef358874e627457149
Paninee K <zphingphong@gmail.com>
2019-12-03 11:40:55 +0800

    add index for localized_texts

--- migrations/migration_047.sql
+++ migrations/migration_047.sql
@@ -4,3 +4,10 @@
 
 CREATE INDEX idx_things_id_type
 ON things (id,type)
+
+---
+--- Create index for localized_texts
+---
+
+CREATE INDEX idx_localized_text_language_things
+ON localized_texts (language,thingid)
