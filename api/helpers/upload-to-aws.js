8233ce2ea6d57fa95a01b6219d8e45e830fbdab1
Paninee K <zphingphong@gmail.com>
2020-01-27 23:52:18 -0800

    Using pure variable, no need to translate

--- api/helpers/upload-to-aws.js
+++ api/helpers/upload-to-aws.js
@@ -15,7 +15,7 @@ function uploadToAWS(base64String) {
   const contentType = base64String.split(":")[1].split(";")[0];
 
   const uploadParams = {
-    Bucket: `${process.env.AWS_S3_BUCKET}`,
+    Bucket: process.env.AWS_S3_BUCKET,
     Key: newFileName,
     Body: base64Data,
     ContentEncoding: "base64",
