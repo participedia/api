b4ef004466346b86dcc0668bff78051db01187e7
Paninee K <zphingphong@gmail.com>
2020-01-28 18:04:53 -0800

    Download all assets fropm old S3

--- scripts/copy-optimize-images-to-new-bucket.js
+++ scripts/copy-optimize-images-to-new-bucket.js
@@ -1,7 +1,31 @@
 const uploadToAWS = require("../api/helpers/upload-to-aws.js");
 const AWS = require("aws-sdk");
 const jimp = require('jimp');
-const s3 = new AWS.S3();
+const s3 = new AWS.S3({
+  apiVersion: "2006-03-01",
+  region: process.env.AWS_REGION,
+  accessKeyId: "AKIAJZHWJQ2EEYB5TRWA",
+  secretAccessKey: "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"
+});
+
+let downloadedFileCount = 0, lastDownloadedFile;
+
+function downloadAssets(){
+  s3.listObjectsV2({
+    Bucket: "uploads.participedia.xyz",
+    StartAfter: lastDownloadedFile
+  }, (err, assetsList) => {
+    assetsList.Contents.forEach(asset => {
+      downloadedFileCount++;
+      lastDownloadedFile = asset.Key;
+      console.log(lastDownloadedFile);
+      console.log(downloadedFileCount);
+    });
+    if(assetsList.IsTruncated) {
+      downloadAssets();
+    };
+  });
+};
 
 jimp.read('https://s3.amazonaws.com/participedia.stage/0bdb532c-04a6-44da-939b-3b91479894c4', (err, img) => {
   if (err) throw err;
@@ -14,8 +38,13 @@ jimp.read('https://s3.amazonaws.com/participedia.stage/0bdb532c-04a6-44da-939b-3
   img
     .scaleToFit(resizeW, resizeH) // resize
     .quality(60) // set JPEG quality
-    .write('test.jpg'); // save
+    .getBase64(jimp.MIME_JPEG, (err, imgData) => {
+      if (err) throw err;
+      console.log(imgData);
+    }); // save
 });
+
+downloadAssets();
 
 
 
