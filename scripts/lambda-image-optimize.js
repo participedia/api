27ac631b52ed34273d2e9cd0305a1d0f82926e4b
Paninee K <zphingphong@gmail.com>
2020-02-19 13:10:24 -0800

    We need to copy over the non-images file anyways

--- scripts/lambda-image-optimize.js
+++ scripts/lambda-image-optimize.js
@@ -66,7 +66,7 @@ exports.handler = function(event, context, callback) {
       let contentType = data.ContentType;
       console.log(contentType);
 
-      if (contentType === "image/png" || contentType === "image/jpg" || contentType === "image/jpeg") { // is image
+      if (["image/png", "image/jpg", "image/jpeg", "image/gif", "image/tiff", "image/bmp"].includes(contentType)) { // is image
         // large image resize
         optimizeImage(rawUrl, contentType, 1600, (err, lBuffer) => { // Optimize and upload thumbnail
           if(err){
@@ -107,6 +107,25 @@ exports.handler = function(event, context, callback) {
         });
       } else {
         console.log('NOT AN IMAGE');
+        // s3.copyObject( // Put object in root folder and thumbnail
+        //   {
+        //     Bucket: bucket,
+        //     CopySource: key,
+        //     ContentType: object.ContentType,
+        //     ACL: "public-read",
+        //     MetadataDirective: "COPY",
+        //     Key: key.split('/')[1],
+        //   }, (err) => {
+        //     if(err){
+        //       console.error(err);
+        //     } else {
+        //       callback(null, {
+        //         statusCode: 200,
+        //         body: event.Records[0].s3.object.key
+        //       });
+        //     }
+        //   }
+        // );
         callback(null, {
           statusCode: 200,
           body: event.Records[0].s3.object.key
