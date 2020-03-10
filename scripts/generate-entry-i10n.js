0305458495ac6aa7961a9af947e01c77fbcf2469
Paninee K <zphingphong@gmail.com>
2020-03-10 15:22:03 -0700

    Prepare google translation

--- scripts/generate-entry-i10n.js
+++ scripts/generate-entry-i10n.js
@@ -9,6 +9,9 @@ const { SUPPORTED_LANGUAGES } = require("./../constants.js");
 const { find } = require("lodash");
 const { db, CASE_BY_ID } = require("../api/helpers/db");
 
+const {Translate} = require('@google-cloud/translate').v2;
+const translate = new Translate({projectId: process.env.GOOGLE_PROJECT_ID});
+
 // getThings();
 getLocalizationData(5243);
 
@@ -28,32 +31,47 @@ function getLocalizationData(thingid) {
   db.any(`SELECT * FROM localized_texts WHERE thingid = ${thingid} ORDER BY timestamp DESC LIMIT 1`)
     .then(function(data) {
       data.forEach(data => {
+        console.log(data);
         var records = [];
         SUPPORTED_LANGUAGES.forEach(language => {
-          if (language.twoLetterCode !== 'en') {
-            // TODO: Implement google translate here
-            records.push({
-              body: 'test',
-              title: 'test',
-              description: 'test',
-              language: language.twoLetterCode,
-              thingid: thingid
-            });
-          }
+          // console.log(language);
+          // if (language.twoLetterCode !== 'en') {
+          //   // TODO: Implement google translate here
+          //   records.push({
+          //     body: 'test',
+          //     title: 'test',
+          //     description: 'test',
+          //     language: language.twoLetterCode,
+          //     thingid: thingid
+          //   });
+          // }
         });
 
-        const insert = pgp.helpers.insert(records, ['body', 'title', 'description', 'language', 'thingid'], 'localized_texts');
+        // const insert = pgp.helpers.insert(records, ['body', 'title', 'description', 'language', 'thingid'], 'localized_texts');
 
-        db.none(insert)
-          .then(function(data) {
-            console.log(data);
-          })
-          .catch(function(error) {
-            console.log(error);
-          });
+        // db.none(insert)
+        //   .then(function(data) {
+        //     console.log(data);
+        //   })
+        //   .catch(function(error) {
+        //     console.log(error);
+        //   });
       });
     })
     .catch(function(error) {
       console.log(error);
     });
+}
+
+async function translateText(data, targetLanguage) {
+  // The text to translate
+  const text = data;
+
+  // The target language
+  const target = targetLanguage;
+
+  const [translation] = await translate.translate(text, target);
+  // console.log(`Text: ${text}`);
+  // console.log(`Translation: ${translation}`);
+  return translation;
 }
