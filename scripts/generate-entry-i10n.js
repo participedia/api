5fd7bb23d3646904ca8f24766b00409dd301ddc0
Paninee K <zphingphong@gmail.com>
2020-03-10 16:09:59 -0700

    Finalized script to translate localized fields

--- scripts/generate-entry-i10n.js
+++ scripts/generate-entry-i10n.js
@@ -12,8 +12,7 @@ const { db, CASE_BY_ID } = require("../api/helpers/db");
 const {Translate} = require('@google-cloud/translate').v2;
 const translate = new Translate({projectId: process.env.GOOGLE_PROJECT_ID});
 
-// getThings();
-getLocalizationData(5243);
+getThings();
 
 function getThings() {
   db.any(`SELECT * FROM things WHERE type IN ('case','method','organization')`)
@@ -31,32 +30,48 @@ function getLocalizationData(thingid) {
   db.any(`SELECT * FROM localized_texts WHERE thingid = ${thingid} ORDER BY timestamp DESC LIMIT 1`)
     .then(function(data) {
       data.forEach(data => {
-        console.log(data);
-        var records = [];
-        SUPPORTED_LANGUAGES.forEach(language => {
-          // console.log(language);
-          // if (language.twoLetterCode !== 'en') {
-          //   // TODO: Implement google translate here
-          //   records.push({
-          //     body: 'test',
-          //     title: 'test',
-          //     description: 'test',
-          //     language: language.twoLetterCode,
-          //     thingid: thingid
-          //   });
-          // }
-        });
-
-        // const insert = pgp.helpers.insert(records, ['body', 'title', 'description', 'language', 'thingid'], 'localized_texts');
-
-        // db.none(insert)
-        //   .then(function(data) {
-        //     console.log(data);
-        //   })
-        //   .catch(function(error) {
-        //     console.log(error);
-        //   });
+        createNewRecord(data, thingid);
       });
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}
+
+async function createNewRecord(data, thingid) {
+  var records = [];
+  for (var i = 0; i < SUPPORTED_LANGUAGES.length; i++) {
+    const language = SUPPORTED_LANGUAGES[i];
+
+    if (language.twoLetterCode !== data.language) {
+      const item = {
+        body: '',
+        title: '',
+        description: '',
+        language: language.twoLetterCode,
+        thingid: thingid
+      };
+
+      if (data.body) {
+        item.body = await translateText(data.body, language.twoLetterCode);
+      }
+
+      if (data.title) {
+        item.title = await translateText(data.title, language.twoLetterCode);
+      }
+
+      if (data.description) {
+        item.description = await translateText(data.description, language.twoLetterCode);
+      }
+
+      records.push(item);
+    }
+  }
+  const insert = pgp.helpers.insert(records, ['body', 'title', 'description', 'language', 'thingid'], 'localized_texts');
+
+  db.none(insert)
+    .then(function(data) {
+      console.log(data);
     })
     .catch(function(error) {
       console.log(error);
