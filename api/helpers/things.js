8ec471e5a5be2b3c4ec21dfb6ce3eeb1595f67b3
Paninee K <zphingphong@gmail.com>
2020-04-28 18:00:00 -0700

    Check url first if string

--- api/helpers/things.js
+++ api/helpers/things.js
@@ -321,6 +321,10 @@ const validateUrl = article => {
 };
 
 const isValidURL = string => {
+  if (typeof string !== 'string') {
+    return false;
+  }
+
   let res = string.match(
     /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g
   );
