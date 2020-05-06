412188a288eb022bfa66ecdf83e277b3a7d8a026
Paninee K <zphingphong@gmail.com>
2020-05-06 16:10:14 -0700

    refactor language redirection

--- public/js/language-select.js
+++ public/js/language-select.js
@@ -1,7 +1,11 @@
 const toArray = nodelist => Array.prototype.slice.call(nodelist);
+const things = ['method', 'case', 'organization'];
 
 const languageSelect = {
+  redirectUrl: null,
+  isThingDetailsPageWithLanguageParam: false,
   init() {
+    this.generateRedirectPath();
     const selectEls = document.querySelectorAll(".js-language-select");
 
     if (!selectEls) return;
@@ -12,10 +16,20 @@ const languageSelect = {
   },
 
   handleSelectChange(e) {
+    let redirectUrl = this.isThingDetailsPageWithLanguageParam ? `${this.redirectUrl}/${e.target.value}` : this.redirectUrl;
     location.href =
       `/set-locale?locale=${e.target.value}` +
-      `&redirectTo=${window.location.pathname}`;
+      `&redirectTo=${redirectUrl}`;
   },
+
+  generateRedirectPath() {
+    this.redirectUrl = window.location.pathname;
+    let urlPathMeta = window.location.pathname.split('/');
+    if (urlPathMeta[1] && things.indexOf(urlPathMeta[1]) >= 0 && urlPathMeta[3]) {
+      this.redirectUrl = `/${urlPathMeta[1]}/${urlPathMeta[2]}`;
+      this.isThingDetailsPageWithLanguageParam = true;
+    }
+  }
 };
 
 export default languageSelect;
