f68f362a6d89f098e692f64d9ba81055455a582d
Alanna Scott <130878+ascott@users.noreply.github.com>
2020-05-30 17:29:25 -0700

    update faq and modal text

--- api/helpers/handlebars-helpers.js
+++ api/helpers/handlebars-helpers.js
@@ -300,12 +300,18 @@ module.exports = {
     );
   },
 
-  originalLanguageModalBody: (context) => {
-    return "<p>" + 
-      `${i18n("how_does_translation_work_modal_body_1", context)}` +
-      "</p>" + 
-      "<p>" + 
-      `${i18n("how_does_translation_work_modal_body_2", context)}`+
+  originalLanguageModalBody: context => {
+    return "<p>" +
+      `${i18n("how_does_translation_work_modal_p1", context)}` +
+      "</p>" +
+      "<p>" +
+      `${i18n("how_does_translation_work_modal_p2", context)}` +
+      "</p>" +
+      "<p>" +
+      `${i18n("how_does_translation_work_modal_p3", context)}` +
+      "</p>" +
+      "<p>" +
+      `${i18n("how_does_translation_work_modal_p4", context)}` +
       "</p>";
   },
 
