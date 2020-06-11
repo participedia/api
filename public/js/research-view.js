605236b231de708520cfab45572d345a57e0fcbd
Alanna Scott <130878+ascott@users.noreply.github.com>
2020-06-10 18:22:16 -0700

    update research page with real-time data downloads section

---
+++ public/js/research-view.js
@@ -0,0 +1,13 @@
+function openAllAccordionsOnPage() {
+  const accordionInputs = Array.prototype.slice.call(
+    document.querySelectorAll(".js-accordion input")
+  );
+  accordionInputs.forEach(input => input.checked = true);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  if (window.innerHeight > 600) {
+    // open all accordion on page load if it's not a mobile/small screen
+    openAllAccordionsOnPage();
+  }
+});
