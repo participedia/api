3f2292b961a79adacb1f7d83063dfb109dfdabbe
Paninee K <zphingphong@gmail.com>
2020-07-28 17:22:33 -0700

    Prepare medium API call response

--- api/controllers/blog-post.js
+++ api/controllers/blog-post.js
@@ -1,9 +1,37 @@
 "use strict";
 const express = require("express");
 const router = express.Router();
+const https = require('https');
 
 router.get("/", async (req, res) => {
-  return res.status(200).json({ OK: true, msg: 'Hello World!' });
+	const accessToken = '2efa4f5dcb2b46e326a8757041bb6d7737227e1047136a25a2f075b1376c5e8eb';
+	const options = {
+		headers: {
+			Authorization: `Bearer ${accessToken}`,
+		},
+		hostname: 'api.medium.com',
+		path: '/v1/me',
+	};
+
+  https.get(options, r => {
+    r.setEncoding('utf8');
+    let body = '';
+    r.on('data', data => {
+      body += data;
+    });
+    r.on('end', () => {
+      try {
+      	res.status(200).json(JSON.parse(body));
+      } catch(e) {
+      	console.error(e);
+      	res.status(200).json(JSON.parse(e));
+      }
+    });
+    r.on('error', error => {
+    	console.error(error);
+      res.status(200).json(error);
+    });
+  });
 });
 
 module.exports = router;
