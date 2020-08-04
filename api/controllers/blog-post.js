7211f8bbebf2ef6165ec19116de50b20395df7aa
Paninee K <zphingphong@gmail.com>
2020-08-04 14:59:04 -0700

    Prepare blog post router to return blog post from medium feed

--- api/controllers/blog-post.js
+++ api/controllers/blog-post.js
@@ -2,35 +2,12 @@
 const express = require("express");
 const router = express.Router();
 const https = require('https');
+const feed = require('rss-to-json');
+
 
 router.get("/", async (req, res) => {
-	const accessToken = '2efa4f5dcb2b46e326a8757041bb6d7737227e1047136a25a2f075b1376c5e8eb';
-	const options = {
-		headers: {
-			Authorization: `Bearer ${accessToken}`,
-		},
-		hostname: 'api.medium.com',
-		path: '/v1/users/18f729917fbdb970c2554947447fe880744d837d8e885efa9a26a1e9c1fb5f614/publications',
-	};
-
-  https.get(options, r => {
-    r.setEncoding('utf8');
-    let body = '';
-    r.on('data', data => {
-      body += data;
-    });
-    r.on('end', () => {
-      try {
-      	res.status(200).json(JSON.parse(body));
-      } catch(e) {
-      	console.error(e);
-      	res.status(200).json(JSON.parse(e));
-      }
-    });
-    r.on('error', error => {
-    	console.error(error);
-      res.status(200).json(error);
-    });
+  feed.load('https://medium.com/feed/@participediaproject').then(rss => {
+    res.status(200).json(rss.items);
   });
 });
 
