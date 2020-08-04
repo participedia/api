8266f54a77f46c9161c4d3646048e7d324955b5f
Paninee K <zphingphong@gmail.com>
2020-08-04 15:07:29 -0700

    return 10 blogs

--- api/controllers/blog-post.js
+++ api/controllers/blog-post.js
@@ -3,11 +3,12 @@ const express = require("express");
 const router = express.Router();
 const https = require('https');
 const feed = require('rss-to-json');
-
+const { chunk } = require("lodash");
 
 router.get("/", async (req, res) => {
   feed.load('https://medium.com/feed/@participediaproject').then(rss => {
-    res.status(200).json(rss.items);
+    const blogItems = rss.items.length > 0 ? chunk(rss.items, 10)[0] : [];
+    res.status(200).json(blogItems);
   });
 });
 
