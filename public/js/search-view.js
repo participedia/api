de398bcff188bfe0e2c82b4c196439644ba9ad66
Paninee K <zphingphong@gmail.com>
2020-08-29 05:14:17 -0700

    Prepare auto complete for search filter country

--- public/js/search-view.js
+++ public/js/search-view.js
@@ -2,8 +2,9 @@ import tabsWithCards from "./tabs-with-cards.js";
 import bookmarkButtons from "./bookmark-buttons.js";
 import dropdownButton from "./dropdown-button.js";
 import searchFilters from "./search-filters.js";
-import searchFilterList from "./search-filter-list.js";
 import lazyLoadImages from "./lazy-load-images.js";
+import searchFilterAutocomplete from "./search-filter-autocomplete.js";
+import searchFilterList from "./search-filter-list.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   tabsWithCards.init();
@@ -11,5 +12,6 @@ document.addEventListener("DOMContentLoaded", () => {
   dropdownButton.init();
   searchFilters.init();
   lazyLoadImages.init();
+  searchFilterAutocomplete.init();
   searchFilterList.init();
 });
