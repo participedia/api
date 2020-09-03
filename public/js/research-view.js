cd4e4026db4b94ce8878761db48257470b22e549
Alanna Scott <130878+ascott@users.noreply.github.com>
2020-09-02 18:25:04 -0700

    fix research links, update some content

--- public/js/research-view.js
+++ public/js/research-view.js
@@ -2,12 +2,18 @@ function openAllAccordionsOnPage() {
   const accordionInputs = Array.prototype.slice.call(
     document.querySelectorAll(".js-accordion input")
   );
-  accordionInputs.forEach(input => input.checked = true);
+  accordionInputs.forEach(input => (input.checked = true));
 }
 
 document.addEventListener("DOMContentLoaded", () => {
   if (window.innerHeight > 600) {
     // open all accordion on page load if it's not a mobile/small screen
     openAllAccordionsOnPage();
+  }
+
+  if (location.hash === "#data-dl") {
+    document.querySelector(".js-data-dl").scrollIntoView({
+      block: "start",
+    });
   }
 });
