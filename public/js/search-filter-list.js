71fbb6d883da7d7dfdec4f9cccf0b33801aa48ff
Paninee K <zphingphong@gmail.com>
2020-09-05 06:41:47 -0700

    Prepare selected filters for search

--- public/js/search-filter-list.js
+++ public/js/search-filter-list.js
@@ -5,9 +5,12 @@ import {
 } from "./utils/utils.js";
 import searchFiltersList from "../../api/helpers/search-filters-list.js";
 
+const toArray = nodeList => Array.prototype.slice.call(nodeList);
+
 const searchFilterList = {
 	init() {
 		this.searchFiltersFormEl = document.querySelector(".js-search-filter-list");
+    this.submitBtnEl = document.querySelector(".js-search-filter-modal-show-result-btn");
 
 		if (!this.searchFiltersFormEl) return;
 
@@ -18,36 +21,128 @@ const searchFilterList = {
     });
 
 		this.checkboxEls = toArray(
-      this.searchFiltersFormEl.querySelectorAll("input[type=checkbox]")
+      this.searchFiltersFormEl.querySelectorAll(".js-keys-list-item input[type=checkbox]")
     );
 
-    this.searchFiltersFormEl.addEventListener("submit", e => {
+    this.submitBtnEl.addEventListener("click", e => {
       this.handleFormSubmit(e);
     });
 
-		const showAllBtn = Array.prototype.slice.call(
-	    document.querySelectorAll(".js-filter-list-item-show-all-btn")
-	  );
 
-		showAllBtn.forEach(el => {
-      el.addEventListener("click", e => this.handleShowAllButtonClick(e));
-    });
 	},
 
-	handleShowAllButtonClick(e) {
-    e.preventDefault();
-    const listKey = e.target.getAttribute("data-list-key");
-    const listItem = Array.prototype.slice.call(
-    	document.querySelector(`.js-keys-list[data-field-name=${listKey}`).children
-    );
+  // handleRemoveSelectedFilter(e) {
+  //   e.preventDefault();
+  //   // Get DOM of clicked item
+  //   const filterEl = document.querySelector(
+  //     `.item-${e.target.dataset.sectionKey}`
+  //   );
 
-    listItem.forEach(el => {
-    	let item = el.classList.contains("hidden");
-    	if (item) {
-    		el.className = "show";
-    	}
+  //   // Remove DOM
+  //   filterEl.remove();
+
+  //   // Get the remaining selected filters
+  //   removeUrlParams(this.SEARCH_FILTER_KEYS);
+  //   const selectedFilters = this.getSelectedFilterState();
+
+  //   // add new filters as params
+  //   Object.keys(selectedFilters).forEach(key => {
+  //     updateUrlParams(key, selectedFilters[key]);
+  //   });
+
+  //   // load new url
+  //   location.href = location.href;
+  // },
+
+  getState() {
+    const selectedFilters = {};
+    this.checkboxEls.forEach(el => {
+      if (el.checked) {
+        const fieldName = el.getAttribute("data-field-name");
+        const filterName = el.getAttribute("name");
+        if (Array.isArray(selectedFilters[fieldName])) {
+          selectedFilters[fieldName].push(filterName);
+        } else {
+          selectedFilters[fieldName] = [filterName];
+        }
+      }
     });
+    return selectedFilters;
   },
+
+  // getSelectedFilterState() {
+  //   this.selectedFilterBtnEl = toArray(
+  //     this.selectedFilterEl.querySelectorAll(".js-remove-selected-filter-btn")
+  //   );
+
+  //   const selectedFilters = {};
+  //   this.selectedFilterBtnEl.forEach(el => {
+  //     const fieldName = el.getAttribute("data-field-name");
+  //     const filterName = el.getAttribute("data-section-key");
+  //     if (Array.isArray(selectedFilters[fieldName])) {
+  //       selectedFilters[fieldName].push(filterName);
+  //     } else {
+  //       selectedFilters[fieldName] = [filterName];
+  //     }
+  //   });
+  //   return selectedFilters;
+  // },
+
+  // updateUIFromUrlParams() {
+  //   const paramsFromUrl = {};
+  //   window.location.search
+  //     .split("?")[1]
+  //     .split("&")
+  //     .map(p => p.split("="))
+  //     .forEach(param => (paramsFromUrl[param[0]] = param[1]));
+
+  //   Object.keys(paramsFromUrl).forEach(key => {
+  //     const values = paramsFromUrl[key].split(",");
+  //     values.forEach(value => {
+  //       const input = document.getElementById(`${key}[${value}]`);
+  //       if (input) input.checked = true;
+  //     });
+  //   });
+  //   this.updateChipButtonsState();
+  // },
+
+  // updateChipButtonsState() {
+  //   this.chipButtonEls.forEach(el => {
+  //     const hasCheckedItems =
+  //       el
+  //         .closest(".js-search-filters-chip-list-item")
+  //         .querySelectorAll("input:checked").length > 0;
+  //     if (hasCheckedItems) {
+  //       el.classList.add("search-filters-chip-selected");
+  //     } else {
+  //       el.classList.remove("search-filters-chip-selected");
+  //     }
+  //   });
+  // },
+
+  // handleClearAllClick(e) {
+  //   e.preventDefault();
+  //   const allCheckboxes = toArray(
+  //     this.searchFiltersFormEl.querySelectorAll("input:checked")
+  //   );
+  //   allCheckboxes.forEach(el => (el.checked = false));
+  //   this.updateChipButtonsState();
+  //   removeUrlParams(this.SEARCH_FILTER_KEYS);
+  //   // load new url without filter
+  //   location.href = location.href;
+  // },
+
+  // handleClearAllForSection(e) {
+  //   e.preventDefault();
+  //   const category = e.target.getAttribute("data-field-name");
+  //   const allCheckboxesForSection = toArray(
+  //     this.searchFiltersFormEl
+  //       .querySelector(`.js-keys-list[data-field-name=${category}`)
+  //       .querySelectorAll("input")
+  //   );
+  //   allCheckboxesForSection.forEach(el => (el.checked = false));
+  //   removeUrlParams(this.SEARCH_FILTER_KEYS);
+  // },
 
   handleFormSubmit(e) {
     e.preventDefault();
@@ -61,6 +156,57 @@ const searchFilterList = {
     // load new url
     location.href = location.href;
   },
+
+  // handleChipButtonClick(e) {
+  //   e.preventDefault();
+
+  //   const allPopOvers = toArray(
+  //     document.querySelectorAll(".js-filter-list-pop-over")
+  //   );
+  //   const currentPopOverEl = e.target
+  //     .closest("li")
+  //     .querySelector(".js-filter-list-pop-over");
+  //   const currentButton = e.target;
+
+  //   // close all other open pop overs before opening the current clicked one
+  //   allPopOvers.forEach(el => {
+  //     if (el !== currentPopOverEl) {
+  //       el.classList.remove("show-filter-list-popover");
+  //     }
+  //   });
+
+  //   // remove open class from chip button before opening a new one
+  //   this.chipButtonEls.forEach(el =>
+  //     el.classList.remove("search-filters-chip-open")
+  //   );
+
+  //   this.updateChipButtonsState();
+
+  //   const xPosOfCurrentButton =
+  //     currentButton.getBoundingClientRect().x +
+  //     currentButton.getBoundingClientRect().width;
+  //   const shouldRenderOnRight = window.innerWidth - xPosOfCurrentButton < 300;
+  //   const isMobile = window.innerWidth < 801;
+
+  //   // toggle current popover if you click the same chip button again
+  //   if (currentPopOverEl.classList.contains("show-filter-list-popover")) {
+  //     // hide popover
+  //     currentButton.classList.remove("search-filters-chip-open");
+  //     currentPopOverEl.classList.remove("show-filter-list-popover");
+  //     currentPopOverEl.style.marginLeft = 0;
+  //     this.updateChipButtonsState();
+  //     document.activeElement.blur();
+  //   } else {
+  //     // show popover
+  //     currentButton.classList.add("search-filters-chip-open");
+  //     currentPopOverEl.classList.add("show-filter-list-popover");
+  //     if (shouldRenderOnRight && !isMobile) {
+  //       const popOverOffset =
+  //         currentPopOverEl.clientWidth - currentButton.clientWidth;
+  //       currentPopOverEl.style.marginLeft = `-${popOverOffset}px`;
+  //     }
+  //   }
+  // }
 };
 
 export default searchFilterList;
