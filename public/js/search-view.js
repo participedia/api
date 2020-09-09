466b1628bf0880b1b345ba9d0d8e8a6caa2597f6
Paninee K <zphingphong@gmail.com>
2020-09-09 11:46:43 -0700

    Remove old search filter html and javascript file

--- public/js/search-view.js
+++ public/js/search-view.js
@@ -1,7 +1,6 @@
 import tabsWithCards from "./tabs-with-cards.js";
 import bookmarkButtons from "./bookmark-buttons.js";
 import dropdownButton from "./dropdown-button.js";
-// import searchFilters from "./search-filters.js";
 import lazyLoadImages from "./lazy-load-images.js";
 import searchFilterList from "./search-filter-list.js";
 
@@ -9,7 +8,6 @@ document.addEventListener("DOMContentLoaded", () => {
   tabsWithCards.init();
   bookmarkButtons.init();
   dropdownButton.init();
-  // searchFilters.init();
   lazyLoadImages.init();
   searchFilterList.init();
 });
