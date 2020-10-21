3711cc5a4a3a8ec4c749044f465dc0ad5e759012
Alanna Scott <130878+ascott@users.noreply.github.com>
2020-10-21 12:30:18 -0700

    fix duplicate blog title bug

--- api/controllers/blog-post.js
+++ api/controllers/blog-post.js
@@ -11,13 +11,24 @@ router.get("/", async (req, res) => {
     .then(rss => {
       const chunkItems = rss.items.length > 0 ? chunk(rss.items, 10)[0] : [];
       const regEx = /<img[^>]+src="?([^"\s]+)"?\s*\/>/;
+      
+      // there is a bug in the medium feed where the title for the first blog post
+      // is returned with the content as well. As a stop gap bug fix i'm hardcoding 
+      // the title here and removing it from the content so we don't see t
+      // he duplicated title on the home page
+      const duplicateTitleInContent = "<h3><strong>Governance Snapshots: <br>Adaptations, Innovations and Practitioner Learning in a Time of COVID-19</strong></h3>";
+
       const blogItems = chunkItems.map(x => {
+        let content = x.content;
+        if (x.content.indexOf(duplicateTitleInContent) === 0) {
+          content = x.content.split(duplicateTitleInContent)[1];
+        }
         return {
           id: x.id,
           title: x.title,
           author: x.author,
           createdAt: x.created,
-          description: x.content.substring(0, 320),
+          description: content.substring(0, 320),
           url: x.url,
           imageUrl: x.content.match(regEx) ? x.content.match(regEx)[1] : null
         };
