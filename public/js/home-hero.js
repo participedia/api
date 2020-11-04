eddcd83914d447be983cf3238a3a04caf6cb283c
Alanna Scott <130878+ascott@users.noreply.github.com>
2020-11-04 14:40:33 -0800

    check for existance of elements before doing hero update. since its in a settimeout there is a race condition when the dom unlloads and js tries to edit it

--- public/js/home-hero.js
+++ public/js/home-hero.js
@@ -3,19 +3,27 @@ import carouselNavigation from "./carousel-navigation.js";
 const homeHero = {
   init() {
     this.heroEl = document.querySelector(".js-home-hero");
-    this.heroFeatures = JSON.parse(this.heroEl.getAttribute("data-hero-features"));
+    this.heroFeatures = JSON.parse(
+      this.heroEl.getAttribute("data-hero-features")
+    );
     this.heroOverlayEl = document.querySelector(".js-home-hero-overlay");
     this.heroImageEl = document.querySelector(".js-home-hero-image");
     this.headerEl = document.querySelector(".js-header");
-    this.heroEntryLinkEl = document.querySelector(".js-home-hero-image-credit__entry-link"); 
-    this.heroCreditEl = document.querySelector(".js-home-hero-image-credit"); 
-    this.heroCountryEl = document.querySelector(".js-home-hero-image-credit__country"); 
-    this.heroCreditTextEl = document.querySelector(".js-home-hero-image-credit__credit"); 
-    
+    this.heroEntryLinkEl = document.querySelector(
+      ".js-home-hero-image-credit__entry-link"
+    );
+    this.heroCreditEl = document.querySelector(".js-home-hero-image-credit");
+    this.heroCountryEl = document.querySelector(
+      ".js-home-hero-image-credit__country"
+    );
+    this.heroCreditTextEl = document.querySelector(
+      ".js-home-hero-image-credit__credit"
+    );
+
     this.currentIndex = 0;
 
     window.addEventListener("resize", () => this.adjustHeight());
-    
+
     this.adjustHeight();
     this.preloadImages();
     this.initSlideshow();
@@ -34,10 +42,10 @@ const homeHero = {
       carouselNavigation.updateCurrentIndex(this.currentIndex);
       this.updateHero(this.currentIndex);
     }, 9000);
-    
+
     const carouselNav = Object.create(carouselNavigation);
-    carouselNav.init({ 
-      numItems: this.heroFeatures.length, 
+    carouselNav.init({
+      numItems: this.heroFeatures.length,
       shouldShowArrows: false,
       el: this.heroEl,
       onChange: index => {
@@ -45,7 +53,7 @@ const homeHero = {
           clearInterval(slideshowId);
         }
         this.updateHero(index);
-      }        
+      },
     });
   },
 
@@ -64,42 +72,54 @@ const homeHero = {
     this.heroCreditEl.style.opacity = 0;
     const newEntry = this.heroFeatures[index];
 
+    const shouldUpdate =
+      this.heroImageEl &&
+      this.heroEntryLinkEl &&
+      this.heroCreditTextEl &&
+      this.heroCountryEl &&
+      this.heroEntryLinkEl &&
+      this.heroOverlayEl &&
+      this.heroCreditEl;
+
     setTimeout(() => {
+      if (!shouldUpdate) return;
+
       // update image
       this.heroImageEl.style.backgroundImage = `url(${newEntry.imageUrl})`;
       // update title
       this.heroEntryLinkEl.innerText = newEntry.entryTitle;
       // update credit
       if (newEntry.imageCredit) {
-        this.heroCreditTextEl.innerText = `Image Credit: ${newEntry.imageCredit}`;
+        this.heroCreditTextEl.innerText = `Image Credit: ${
+          newEntry.imageCredit
+        }`;
       } else {
         this.heroCreditTextEl.innerText = "";
       }
-      
+
       // update country
       if (newEntry.country) {
         this.heroCountryEl.innerText = newEntry.country;
       } else {
         this.heroCountryEl.innerText = "";
       }
-      
+
       // update link
-      this.heroEntryLinkEl.setAttribute("href", newEntry.entryUrl)
+      this.heroEntryLinkEl.setAttribute("href", newEntry.entryUrl);
       // fade opacity back up to initial
       this.heroOverlayEl.style.opacity = 0.5;
       this.heroCreditEl.style.opacity = 1;
     }, 250);
   },
 
-  adjustHeight () {
-    // hero image should be the 
+  adjustHeight() {
+    // hero image should be the
     // height of the browser window - the height of the header
     const height = window.innerHeight - this.headerEl.offsetHeight;
     this.heroEl.style.height = `${height}px`;
     this.heroOverlayEl.style.height = `${height}px`;
     this.heroImageEl.style.height = `${height}px`;
-  }
-  
-}
+  },
+};
 
 export default homeHero;
