a934c04cb2253c259d2f0462f0aa34359c98aa7c
Paninee K <zphingphong@gmail.com>
2020-11-27 04:12:13 -0800

    Handle download csv button click

--- public/js/csv-generator.js
+++ public/js/csv-generator.js
@@ -1,5 +1,12 @@
 const csvGenerator = {
-
+	init() {
+		const downloadCsvBtnEl = document.querySelector(".js-download-csv-btn");
+		if (downloadCsvBtnEl) {
+      downloadCsvBtnEl.addEventListener("click", e => {
+      	window.open(window.location.href, '_blank');
+      });
+    }
+	}
 }
 
 export default csvGenerator;
