91e05786303fee3c84311c97a61e6528e0232ab5
paninee <zphingphong@gmail.com>
2021-03-02 22:11:48 +0100

    Processed all database text enrties with AWS comprehend

--- scripts/language-detector.js
+++ scripts/language-detector.js
@@ -23,7 +23,9 @@ const comprehend = new AWS.Comprehend({
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
 
-const numData = 10;
+const numData = -1;
+const maxBatchSize = 25;
+let currentBatched = 0;
 
 detectLangauge = async str => {
   const resCases = await tt(numData, "case");
@@ -35,65 +37,89 @@ detectLangauge = async str => {
     {
       fileName: "case",
       data: resCases,
+      processed: comprehendIt(resCases),
     },
     {
       fileName: "organization",
       data: resOrg,
+      processed: comprehendIt(resOrg),
     },
     {
       fileName: "method",
       data: resMethod,
+      processed: comprehendIt(resMethod),
     },
     {
       fileName: "collections",
       data: resCollection,
+      processed: comprehendIt(resCollection),
     },
   ];
 
-  arr.forEach(thing => {
-    const TextList = thing.data.map(res => res.bodyString);
-    const ids = thing.data.map(res => res.thingID);
-    console.log(ids);
-    var params = {
-      TextList /* required */,
-    };
-
-    comprehend.batchDetectDominantLanguage(params, function(err, data) {
-      if (err) console.log(err, err.stack);
-      else {
-        const cleanedData = data.ResultList.map((d, i) => {
-          return {
-            languageDetected: d.Languages.length
-              ? d.Languages[0].LanguageCode
-              : null,
-            original_language: thing.data[i].original_language,
-            language: thing.data[i].language,
-            score: d.Languages[0].Score,
-            thingID: thing.data[i].thingID,
-          };
-        });
-        writeToCSVFile(
-          cleanedData,
-          [
-            "languageDetected",
-            "original_language",
-            "language",
-            "score",
-            "thingID",
-          ],
-          [
-            "Detected Language",
-            "Original Language",
-            "Language",
-            "Confidence",
-            "Thing ID",
-          ],
-          thing.fileName
-        );
-      }
+  const lists = {};
+  arr.forEach(el => {
+    const flatList = [];
+    el.processed.then(reslt => {
+      console.log(el.fileName);
+      reslt.map(ele => {
+        if (ele.status === "fulfilled") {
+          flatList.push(ele.value.ResultList.map(elem => elem.Languages[0]));
+        }
+      });
+      lists[el.fileName] = flatList;
+      const flattendList = flatList.flat().map((element, i) => {
+        const returnedData = {
+          ...el.data[i],
+          ...element,
+          languageDetected: element.LanguageCode,
+        };
+        delete returnedData["bodyString"];
+        delete returnedData["LanguageCode"];
+        return returnedData;
+      });
+      writeToCSVFile(
+        flattendList,
+        [
+          "languageDetected",
+          "original_language",
+          "language",
+          "Score",
+          "thingID",
+        ],
+        [
+          "Detected Language",
+          "Original Language",
+          "Language",
+          "Confidence",
+          "Thing ID",
+        ],
+        el.fileName
+      );
     });
+    console.log(lists);
   });
+  console.log(lists);
 };
+
+function chunkArr(arr, size) {
+  const chunked = [];
+  for (let i = 0; i < arr.length; i += size) {
+    chunked.push(arr.slice(i, size + i));
+  }
+  return chunked;
+}
+
+async function comprehendIt(data) {
+  const promises = [];
+  const dataArr = chunkArr(data, maxBatchSize);
+  dataArr.forEach(el => {
+    let params = {
+      TextList: el.map(el => el.bodyString),
+    };
+    promises.push(comprehend.batchDetectDominantLanguage(params).promise());
+  });
+  return Promise.allSettled(promises);
+}
 
 async function tt(count = 10, type = "case") {
   const { Client } = require("pg");
@@ -103,7 +129,9 @@ async function tt(count = 10, type = "case") {
   query = `SELECT lt.body, lt.thingid, lt.language, t.original_language, t.type
   FROM localized_texts lt
   LEFT JOIN things t
-  ON t.id = lt.thingid WHERE t.type = '${type}' LIMIT ${count}`;
+  ON t.id = lt.thingid WHERE t.type = '${type}' ${
+    count > -1 ? "LIMIT " + count : ""
+  } `;
 
   return client.query(query).then(res => {
     for (let i = 0; i < res.rows.length; i++) {
@@ -126,10 +154,18 @@ async function writeToCSVFile(data, fields, fieldNames, filename) {
     fields,
     fieldNames,
   };
+  const filePath = "csvs/" + filename + ".csv";
   try {
     const parser = new Parser(opts);
     const csv = parser.parse(data);
-    fs.writeFile("csvs/" + filename + ".csv", csv, res => {});
+    if (fs.existsSync(filePath)) {
+      const parser = new Parser();
+      const csv = parser.parse(data);
+      fs.appendFileSync(filePath, "\n");
+      fs.appendFileSync(filePath, csv);
+    } else {
+      fs.writeFileSync(filePath, csv);
+    }
   } catch (err) {
     console.error(err);
   }
