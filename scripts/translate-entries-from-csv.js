1784688f54ccca8831bbf3685b3f1d0503fa2110
paninee <zphingphong@gmail.com>
2021-03-17 13:28:05 +0100

    Implementing copy script.

--- scripts/translate-entries-from-csv.js
+++ scripts/translate-entries-from-csv.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+const keysEnvVar = process.env["GOOGLE_TRANSLATE_CREDENTIALS"];
 const csvtojsonV2 = require("csvtojson/v2");
 const { Translate } = require("@google-cloud/translate").v2;
 const authKeys = JSON.parse(keysEnvVar);
@@ -5,12 +7,14 @@ authKeys["key"] = process.env.GOOGLE_API_KEY;
 const translate = new Translate(authKeys);
 
 let loadedCSV;
+let highScoreEntries = [];
 const lowScoreEntries = [];
-const keysEnvVar = process.env["GOOGLE_TRANSLATE_CREDENTIALS"];
 const { Client } = require("pg");
 const client = new Client(process.env.DATABASE_URL);
 let loadingCSV = false;
-const itemsToTranslate = 10;
+const itemsToTranslate = 1;
+const fileName = "case";
+const fileToWriteName = "case_report";
 
 if (!keysEnvVar) {
   throw new Error(
@@ -41,26 +45,35 @@ function loadCsvAsJSON(fileName = "case_test") {
 
 function filterHighScoreEntries() {
   if (loadedCSV) {
-    const filtered = jsonObj.filter((el, i) => {
-      return Number(el.Score) > 0.9;
+    const filtered = loadedCSV.filter((el, i) => {
+      return (
+        Number(el.Score) > 0.9 &&
+        el.language === el.original_language &&
+        el.languageDetected !== el.language
+      );
     });
-    lowScoreEntries = filtered;
+    highScoreEntries = filtered;
+    console.log(highScoreEntries);
     return filtered;
   }
   throw new Error("CSV entries not found");
 }
 
-async function saveRecord(records) {
-  const insert = pgp.helpers.insert(
-    records,
-    ["body", "title", "description", "language", "thingid", "timestamp"],
-    "localized_texts"
-  );
-  db.none(insert);
+async function saveRecord(record) {
+  const { body, title, description, language } = record;
+  const query = `UPDATE localized_texts lt SET lt.body = $1, lt.title = $2, lt.description = $3, lt.language = $4 WHERE id = ${record.id} RETURNING *`;
+  client.query(query, [body, title, description, language], (err, res) => {
+    if (err) {
+      console.log(err.stack);
+    } else {
+      console.log("Updated: ", JSON.stringify(res.rows[0]));
+      writeToCSVFile(res.rows[0]);
+    }
+  });
 }
 
-async function getRecordToTranslate(thingID, language) {
-  const query = `SELECT * FROM localized_texts WHERE thingid = ${thingID} AND language = '${language}' ORDER BY timestamp DESC LIMIT 1`;
+async function getRecordToCopy(thingID, language, detected_language) {
+  const query = `SELECT * FROM localized_texts lt LEFT JOIN things t ON t.id = lt.thingid WHERE lt.thingid = ${thingID} AND lt.language = '${language}' AND lt.language != '${detected_language}' ORDER BY timestamp DESC LIMIT 1`;
   return client.query(query).then(
     reslt => {
       return reslt.rows[0];
@@ -79,12 +92,65 @@ async function translateText(text, targetLanguage) {
   return translation;
 }
 
-await client.connect();
-loadCsvAsJSON().then(res => {
-  filterHighScoreEntries();
-  highScoreEntries.splice(0, itemsToTranslate || Infinity).forEach((el, i) => {
-    getRecordToTranslate(el.thingID, el.languageDetected).then(res => {
-      translateText(res.description || res.text, el.language);
-    });
+async function writeToCSVFile(data) {
+  const opts = {
+    fields: [
+      "textSample",
+      "languageDetected",
+      "original_language",
+      "language",
+      "Score",
+      "thingID",
+    ],
+    fieldNames: [
+      "Detected Language",
+      "Original Language",
+      "Language",
+      "Confidence",
+      "Thing ID",
+    ],
+  };
+  const filePath = "csvs/" + fileToWriteName + ".csv";
+  try {
+    let parser;
+    let csv;
+    if (fs.existsSync(filePath)) {
+      parser = new Parser();
+      csv = parser.parse(data);
+      csv = csv.replace(
+        '"textSample","languageDetected","original_language","language","Score","thingID"\n',
+        ""
+      );
+
+      fs.appendFileSync(filePath, `\n${csv}`);
+    } else {
+      parser = new Parser(opts);
+      csv = parser.parse(data);
+      fs.writeFileSync(filePath, csv);
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+function copyEntry(entryData) {
+  saveRecord(entryData);
+}
+
+client.connect().then(res => {
+  loadCsvAsJSON(fileName).then(res => {
+    filterHighScoreEntries();
+    highScoreEntries
+      .splice(0, itemsToTranslate || Infinity)
+      .forEach((el, i) => {
+        getRecordToCopy(
+          el.thingID,
+          el.original_language,
+          el.languageDetected
+        ).then(res => {
+          // translateText(res.description || res.text, el.language);
+          copyEntry(res);
+        });
+      });
   });
 });
