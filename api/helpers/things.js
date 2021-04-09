c48e3c546f162dd8d594b5d2b3ba092c89064139
paninee <zphingphong@gmail.com>
2021-04-09 14:54:33 +0100

    Modified regex to allow for longer TLDs

--- api/helpers/things.js
+++ api/helpers/things.js
@@ -371,7 +371,7 @@ const isValidURL = string => {
   }
 
   let res = string.match(
-    /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g
+    /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,63}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g
   );
   return res !== null;
 };
