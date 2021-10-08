3b9d00b8fa183d3e36adb3a627385f8b18e5c138
paninee <zphingphong@gmail.com>
2021-10-08 14:53:59 +0100

    Update things.js

--- api/helpers/things.js
+++ api/helpers/things.js
@@ -261,7 +261,7 @@ async function maybeUpdateUserTextLocaleEntry(body, req, res, type) {
   }
 
   if (textModified) {
-    return { updatedText, author, newArticle };
+    return { updatedText, author, oldArticle };
   } else {
     return { updatedText: null, author, oldArticle };
   }
