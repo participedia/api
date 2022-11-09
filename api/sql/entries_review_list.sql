6416076d5c13cacb132e83d4e31847c7ac5ab64b
paninee <zphingphong@gmail.com>
2022-11-09 12:13:35 +0700

    add user data to the waiting for review list

--- api/sql/entries_review_list.sql
+++ api/sql/entries_review_list.sql
@@ -1,16 +1,19 @@
 WITH all_hidden_things  AS (
-    select * from things, 
+    SELECT * FROM things, 
     get_localized_texts_fallback(things.id, 'en', things.original_language) AS texts
-    where hidden = true and "type" != 'collection' order by updated_date 
+    WHERE hidden = true and "type" != 'collection' ORDER BY updated_date 
 ),
 all_authors as (
     SELECT DISTINCT ON (thingid) *
     FROM  authors 
     ORDER  BY thingid, "timestamp"  DESC NULLS LAST
+),
+all_users as (
+    SELECT name, id FROM users ORDER BY id desc
 )
-SELECT all_hidden_things.*, all_authors.*
-FROM all_hidden_things, all_authors
-where all_hidden_things.id = all_authors.thingid
+SELECT all_hidden_things.*, all_authors.*, all_users.*
+FROM all_hidden_things, all_authors, all_users
+WHERE all_hidden_things.id = all_authors.thingid and all_authors.user_id = all_users.id
 ORDER BY updated_date  DESC
 OFFSET 1
 LIMIT null
