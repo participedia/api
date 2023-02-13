1034b462a804c7341671d826fa5d14ec4d04a1be
paninee <zphingphong@gmail.com>
2023-02-13 11:36:55 +0700

    update org to review publish entry

--- api/sql/create_organization.sql
+++ api/sql/create_organization.sql
@@ -1,10 +1,10 @@
 WITH insert_organization as (
   INSERT into organizations (
-    type, original_language, post_date, published, updated_date
+    type, original_language, post_date, published, updated_date, hidden
   )
   VALUES
     (
-      'organization', ${original_language}, 'now', true, 'now'
+      'organization', ${original_language}, 'now', true, 'now', ${hidden}
     ) RETURNING id as thingid
 )
 
