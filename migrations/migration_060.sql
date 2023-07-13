481342cc79ff80327fd95e6a78976acb3abe5378
paninee <zphingphong@gmail.com>
2023-07-13 12:09:54 +0700

    update migration script for export csv

--- migrations/migration_060.sql
+++ migrations/migration_060.sql
@@ -1,11 +1,13 @@
---
--- Name: csv_export; Type: TABLE; Schema: public; Owner: -
---
+DROP TABLE csv_export;
 
 CREATE TABLE csv_export (
     id SERIAL PRIMARY KEY,
     requested_timestamp timestamp,
     finished_timestamp timestamp,
     download_url text DEFAULT ''::text,
-    type text NOT NULL
+    type text NOT NULL,
+    created_at timestamp,
+    created_by text DEFAULT ''::text,
+    updated_at timestamp,
+    updated_by text DEFAULT ''::text
 );
