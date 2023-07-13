cd758fd207401eb6f0c4abb6a3f81cf1f302eaca
paninee <zphingphong@gmail.com>
2023-07-13 11:33:31 +0700

    init backend for ecport csv

---
+++ api/controllers/exports.js
@@ -0,0 +1,29 @@
+"use strict";
+let express = require("express");
+let router = express.Router(); // eslint-disable-line new-cap
+
+let { db, ENTRIES_REVIEW_LIST } = require("../helpers/db");
+
+const ManagementClient = require("auth0").ManagementClient;
+
+const auth0Client = new ManagementClient({
+  domain: process.env.AUTH0_DOMAIN,
+  clientId: process.env.AUTH0_CLIENT_ID,
+  clientSecret: process.env.AUTH0_CLIENT_SECRET,
+  scope: "read:users update:users",
+});
+
+const requireAuthenticatedUser = require("../middleware/requireAuthenticatedUser.js");
+
+router.get("/export/csv", requireAuthenticatedUser(), async function(req, res) {
+    const user_query = req.query.query || "";
+    
+    const returnType = req.query.returns || "html";
+    if (returnType === "html") {
+    return res.status(200).render(`csv-exports`, { results });
+    } else if (returnType === "json") {
+    return res.status(200).json(data);
+    }
+  });
+
+module.exports = router;
