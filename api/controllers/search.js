943c3fa6d30e3d70055e94f06f3ba1173b771887
paninee <zphingphong@gmail.com>
2023-08-03 19:17:25 +0700

    remove unused code on search

--- api/controllers/search.js
+++ api/controllers/search.js
@@ -4,7 +4,6 @@ let router = express.Router(); // eslint-disable-line new-cap
 let {
   db,
   as,
-  TITLES_FOR_THINGS,
   LIST_MAP_CASES,
   LIST_MAP_ORGANIZATIONS,
 } = require("../helpers/db");
@@ -23,7 +22,6 @@ const {
   uploadCSVFile
 } = require("../helpers/export-helpers");
 const logError = require("../helpers/log-error.js");
-const { RESPONSE_LIMIT } = require("./../../constants.js");
 const SUPPORTED_LANGUAGES = require("../../constants").SUPPORTED_LANGUAGES;
 
 function randomTexture() {
@@ -32,62 +30,8 @@ function randomTexture() {
 }
 
 function getLanguage(req) {
-  // once we have translations for user generated content in all supported languages,
-  // we can use the locale cookie to query by language.
-  // currently if the locale is set to something other than "en", no results are returned,
-  // so hardcoding "en" here
   return req.cookies.locale || "en";
 }
-
-/**
- *  Deprecated, use /list/* methods instead
- *
- */
-router.get("/getAllForType", async function getAllForType(req, res) {
-  try {
-    let objType = req.query.objType.toLowerCase();
-    let page = Math.max(parseInt(req.query.page || 1), 1);
-    if (Number.isNaN(page)) {
-      page = 1;
-    }
-    let offset = 0;
-    let response_limit = Number.MAX_SAFE_INTEGER;
-    if (!req.query.response_limit) {
-      // do nothing, return everything
-    } else if (
-      req.query.response_limit &&
-      req.query.response_limit.toLowerCase() === "none"
-    ) {
-      response_limit = Number.MAX_SAFE_INTEGER;
-    } else {
-      response_limit = parseInt(req.query.response_limit || RESPONSE_LIMIT);
-      offset = Math.max(page - 1, 0) * response_limit;
-    }
-    if (!supportedTypes.includes(objType)) {
-      res.status(401).json({
-        message: "Unsupported objType for getAllForType: " + objType,
-      });
-    }
-    const titlelist = await db.any(TITLES_FOR_THINGS, {
-      language: as.value(getLanguage(req)),
-      limit: response_limit,
-      offset: offset,
-      type: objType,
-    });
-    let jtitlelist = {};
-    // FIXME: this is a dumb format but it is what front-end expects.
-    // Switch both (and tests) to use array of {title: , id: } pairs.
-    // Also, if we're going to use {OK: true, data: []} everywhere else
-    // we should use it here too.
-    titlelist.forEach(function(row) {
-      jtitlelist[row.title] = Number(row.thingid);
-    });
-    res.status(200).json(jtitlelist);
-  } catch (error) {
-    logError(error);
-    res.status(500).json({ error: error });
-  }
-});
 
 const redirectToSearchPageIfHasCollectionsQueryParameter = (req, res, next) => {
   if (req.query.hasOwnProperty('collections')) {
@@ -129,7 +73,7 @@ const redirectToSearchPageIfHasCollectionsQueryParameter = (req, res, next) => {
 // if there is no query OR the query is "featured" then return all featured items
 // One further item: need an alternative search which returns only map-level items and has no pagination
 
-router.get("/", redirectToSearchPageIfHasCollectionsQueryParameter, async function(req, res) {
+router.get("/", redirectToSearchPageIfHasCollectionsQueryParameter, async (req, res) => {
   const user_query = req.query.query || "";
   const parsed_query = preparse_query(user_query);
   const limit = limitFromReq(req);
@@ -152,10 +96,7 @@ router.get("/", redirectToSearchPageIfHasCollectionsQueryParameter, async functi
       if (req.query.resultType === "map" && parsed_query) {
         results = results.filter(result => result.searchmatched);
       }
-  
-      const total = Number(
-        results.length ? results[0].total || results.length : 0
-      );
+      const total = Number(results.length ? results[0].total || results.length : 0);
       const searchhits = results.filter(result => result.searchmatched).length;
       const pages = Math.max(limit ? Math.ceil(total / limit) : 1, 1); // Don't divide by zero limit, don't return page 1 of 1
       results.forEach(obj => {
@@ -214,32 +155,6 @@ router.get("/", redirectToSearchPageIfHasCollectionsQueryParameter, async functi
       let OK = false;
       res.status(500).json({ OK, error });
     }
-  }
-});
-
-/*
- * Deprecated, use /search/?resultType=map
- *
- */
-router.get("/map", async function(req, res) {
-  try {
-    const RESPONSE_LIMIT = 1000;
-    const offset = 0;
-    const cases = await db.any(LIST_MAP_CASES, {
-      language: as.value(getLanguage(req)),
-      limit: RESPONSE_LIMIT,
-      offset: offset,
-    });
-    const orgs = await db.any(LIST_MAP_ORGANIZATIONS, {
-      language: as.value(getLanguage(req)),
-      limit: RESPONSE_LIMIT,
-      offset: offset,
-    });
-
-    res.status(200).json({ data: { cases, orgs } });
-  } catch (error) {
-    logError(error);
-    res.status(500).json({ error: error });
   }
 });
 
