2a77c5644548b01b60edd08579813a1c63799c87
Paninee Khantidhara <zphingphong@gmail.com>
2024-01-31 19:36:47 +0300

    update the translate script

--- scripts/entires-character-calculator.js
+++ scripts/entires-character-calculator.js
@@ -18,7 +18,7 @@ async function processTranslation() {
       SELECT localized_texts.thingid, localized_texts.title, localized_texts.language, localized_texts.body, things.id, things.original_language, localized_texts.timestamp
       FROM (SELECT * FROM things WHERE things.original_language <> '' GROUP BY id, original_language) as things
       JOIN localized_texts ON things.id = localized_texts.thingid
-      WHERE things.hidden = false AND things.published = true AND (
+      WHERE localized_texts.thingid IN (12489, 12524) AND things.hidden = false AND things.published = true AND (
         localized_texts.title IS NULL OR localized_texts.title = ''
       )
       AND localized_texts.timestamp > '2023-07-01'
