e02419b3b8aee9a55713ec7d7e71b3c8934982b9
Paninee Khantidhara <zphingphong@gmail.com>
2024-09-10 18:38:59 +0300

    add a copy link button to the details page

--- public/js/bookmark-buttons.js
+++ public/js/bookmark-buttons.js
@@ -1,4 +1,5 @@
 import { xhrReq } from "./utils/utils.js";
+import copy from 'copy-to-clipboard';
 
 const bookmarkButtons = {
   init() {
@@ -14,6 +15,21 @@ const bookmarkButtons = {
         this.handleLinkClick(e);
       });
     });
+
+    // copy click action
+    const copyButtons = Array.prototype.slice.call(
+      document.querySelectorAll(".js-copy-button")
+    );
+
+    if (copyButtons.length < 1) return;
+
+    copyButtons.forEach(copyButtonEl => {
+      copyButtonEl.addEventListener("click", e => {
+        e.preventDefault();
+        this.handleCopyClick(e);
+      });
+    });
+
   },
 
   toggleBookmark(addOrDelete, linkEl, isBookmarked, thingid, bookmarkType) {
@@ -56,6 +72,29 @@ const bookmarkButtons = {
     } else {
       this.toggleBookmark("add", linkEl, isBookmarked, thingid, bookmarkType);
     }
+  },
+
+
+  handleCopyClick(e) {
+    e.preventDefault();
+    const linkEl = e.target.closest("a");
+    const copyType = linkEl.getAttribute("data-type");
+    const thingid = linkEl.getAttribute("data-thing-id");
+    const host = window.location.host;
+    const path = `https://${host}/${copyType}/${thingid}`;
+    copy(path);
+    Toastify({
+      text: "Link copied to clipboard!",
+      duration: 3000,
+      close: true,
+      gravity: "top", // `top` or `bottom`
+      position: "right", // `left`, `center` or `right`
+      stopOnFocus: true, // Prevents dismissing of toast on hover
+      style: {
+        background: "#2f3e46",
+      },
+    }).showToast();
+
   },
 };
 
