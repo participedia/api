6c96f5f47b041371ef1d6e7683ca921ba24538b8
Paninee Khantidhara <zphingphong@gmail.com>
2025-01-07 12:11:36 -0800

    build a script to get current database version

---
+++ scripts/psql-version.js
@@ -0,0 +1,21 @@
+require("dotenv").config({ silent: process.env.NODE_ENV === "production" });
+const { db } = require("../api/helpers/db.js");
+
+async function checkVersion() {
+  console.log('*********** START PROCESSING ***********');
+
+  try {
+    const res = await db.one(`SELECT version();`);
+    console.log('Query Result:', res); // Log the full response
+    console.log('PostgreSQL Version:', res.version); // Access the version
+
+    process.exit();
+  } catch (error) {
+    console.log('????????????????? catch error ?????????????? error', error)
+    process.exit();
+  }
+
+}
+
+
+checkVersion();
