7339a6a04561c33b6ba91d73c461bfde5348f054
Paninee Khantidhara <zphingphong@gmail.com>
2025-01-08 20:42:36 +0300

    upgrade the aws sdk to javascript 3

--- api/helpers/upload-to-aws.js
+++ api/helpers/upload-to-aws.js
@@ -1,17 +1,25 @@
-const AWS = require("aws-sdk");
+const { S3Client } = require("@aws-sdk/client-s3");
+const { Upload } = require("@aws-sdk/lib-storage");
 const { v4: uuidv4 } = require("uuid");
 const logError = require("./log-error.js");
 const fs = require('fs');
-const s3 = new AWS.S3({
-  apiVersion: "2006-03-01",
+const s3Client = new S3Client({
   region: process.env.AWS_REGION,
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
 });
+// const s3 = new AWS.S3({
+//   apiVersion: "2006-03-01",
+//   region: process.env.AWS_REGION,
+//   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+//   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+// });
 
 const { ALLOWED_IMAGE_TYPES } = require("../../constants.js");
 
-AWS.config.update({ region: process.env.AWS_REGION });
+// AWS.config.update({ region: process.env.AWS_REGION });
 
 function createBufferFromBase64(base64String) {
   return Buffer.from(
@@ -20,19 +28,19 @@ function createBufferFromBase64(base64String) {
   );
 }
 
-function uploadObject(buffer, contentType, filename, cb) {
-  let key = `raw/${filename}`;
-  const uploadParams = {
-    Bucket: process.env.AWS_S3_BUCKET,
-    Key: key,
-    Body: buffer,
-    ContentEncoding: "base64",
-    ContentType: contentType,
-    ACL: "public-read"
-  };
+// function uploadObject(buffer, contentType, filename, cb) {
+//   let key = `raw/${filename}`;
+//   const uploadParams = {
+//     Bucket: process.env.AWS_S3_BUCKET,
+//     Key: key,
+//     Body: buffer,
+//     ContentEncoding: "base64",
+//     ContentType: contentType,
+//     ACL: "public-read"
+//   };
 
-  s3.upload(uploadParams, cb);
-}
+//   s3.upload(uploadParams, cb);
+// }
 
 const uploadToAWS = (base64String) => {
   const contentType = base64String.split(":")[1].split(";")[0];
@@ -41,39 +49,59 @@ const uploadToAWS = (base64String) => {
   const base64Buffer = createBufferFromBase64(base64String);
   const key = `raw/${newFileName}`;
 
-  const params = {
+  // Prepare upload parameters
+  const uploadParams = {
     Bucket: process.env.AWS_S3_BUCKET,
-    Key: key, 
+    Key: key,
     Body: base64Buffer,
     ContentEncoding: "base64",
     ContentType: contentType,
-    ACL: "public-read"
+    ACL: "public-read",
   };
 
+
   try {
-    const stored = s3.upload(params).promise();
-    uploadedLocation = stored.Location;
+    // Use the Upload helper to handle the put operation
+    const parallelUpload = new Upload({
+      client: s3Client,
+      params: uploadParams,
+    });
+
+    // This promise resolves when upload is complete
+    parallelUpload.done();
+    
+    return `${process.env.AWS_UPLOADS_URL}${newFileName}`;
   } catch (err) {
-    console.log(err)
+    console.log("uploadToAWS ", err)
+    throw err;
   }
 
-  return `${process.env.AWS_UPLOADS_URL}${newFileName}`;
 }
 
 const uploadCSVToAWS = async (files, filename) => {
+  // Read file content from disk
   const fileContent = fs.readFileSync(files);
+  
+  // Construct the S3 upload parameters
   let uploadedLocation = "";
-
   const params = {
     Bucket: process.env.AWS_S3_BUCKET,
-    Key: filename, 
+    Key: filename,
     Body: fileContent,
-    ACL: "public-read"
+    ACL: "public-read",
   };
 
   try {
-    const stored = await s3.upload(params).promise();
-    uploadedLocation = stored.Location;
+    // Use the high-level Upload class (@aws-sdk/lib-storage)
+    const parallelUpload = new Upload({
+      client: s3Client,
+      params,
+    });
+   // Wait for the upload to complete
+    const uploadResult = await parallelUpload.done();
+    // The result has the Location, ETag, etc.
+    uploadedLocation = uploadResult.Location;
+    
   } catch (err) {
     console.log(err)
   }
--- scripts/copy-raw-images.js
+++ scripts/copy-raw-images.js
@@ -1,81 +1,80 @@
-const uploadToAWS = require("../api/helpers/upload-to-aws.js");
-const AWS = require("aws-sdk");
-const jimp = require("jimp");
-const async = require("async");
-const s3 = new AWS.S3({
-  apiVersion: "2006-03-01",
-  region: process.env.AWS_REGION,
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-});
+// const AWS = require("aws-sdk");
+// const jimp = require("jimp");
+// const async = require("async");
+// const s3 = new AWS.S3({
+//   apiVersion: "2006-03-01",
+//   region: process.env.AWS_REGION,
+//   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+//   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+// });
 
-let downloadedFileCount = 0,
-  lastDownloadedFile,
-  isMore = true;
-let oldBucket = "uploads.participedia.xyz";
+// let downloadedFileCount = 0,
+//   lastDownloadedFile,
+//   isMore = true;
+// let oldBucket = "uploads.participedia.xyz";
 
-async.whilst(
-  () => {
-    return isMore;
-  },
-  cb => {
-    s3.listObjectsV2(
-      {
-        Bucket: oldBucket,
-        Delimiter: "/",
-        StartAfter: lastDownloadedFile
-      },
-      (err, assetsList) => {
-        async.eachSeries(
-          assetsList.Contents,
-          (asset, innerCb) => {
-            downloadedFileCount++;
-            lastDownloadedFile = asset.Key;
-            console.log(lastDownloadedFile);
-            console.log(encodeURI(`${oldBucket}/${asset.Key}`));
-            console.log(downloadedFileCount);
-            s3.getObject(
-              {
-                Bucket: oldBucket,
-                Key: lastDownloadedFile,
-              },
-              (err, object) => {
-                console.log(object.ContentType);
+// async.whilst(
+//   () => {
+//     return isMore;
+//   },
+//   cb => {
+//     s3.listObjectsV2(
+//       {
+//         Bucket: oldBucket,
+//         Delimiter: "/",
+//         StartAfter: lastDownloadedFile
+//       },
+//       (err, assetsList) => {
+//         async.eachSeries(
+//           assetsList.Contents,
+//           (asset, innerCb) => {
+//             downloadedFileCount++;
+//             lastDownloadedFile = asset.Key;
+//             console.log(lastDownloadedFile);
+//             console.log(encodeURI(`${oldBucket}/${asset.Key}`));
+//             console.log(downloadedFileCount);
+//             s3.getObject(
+//               {
+//                 Bucket: oldBucket,
+//                 Key: lastDownloadedFile,
+//               },
+//               (err, object) => {
+//                 console.log(object.ContentType);
 
-                // Copy over documents
-                s3.copyObject(
-                  {
-                    Bucket: process.env.AWS_S3_BUCKET,
-                    CopySource: encodeURI(`${oldBucket}/${asset.Key}`),
-                    ContentType: object.ContentType,
-                    ACL: "public-read",
-                    MetadataDirective: "COPY",
-                    Key: `raw/${asset.Key}`,
-                  }, (err) => {
-                    if(err){
-                      console.error(err);
-                    }
-                    innerCb();
-                  }
-                );
-              }
-            );
-          },
-          err => {
-            if (err) {
-              cb(err);
-            } else {
-              if (!assetsList.IsTruncated) {
-                isMore = false;
-              }
-              cb();
-            }
-          }
-        );
-      }
-    );
-  },
-  err => {
-    if (err) throw err;
-  }
-);
+//                 // Copy over documents
+//                 s3.copyObject(
+//                   {
+//                     Bucket: process.env.AWS_S3_BUCKET,
+//                     CopySource: encodeURI(`${oldBucket}/${asset.Key}`),
+//                     ContentType: object.ContentType,
+//                     ACL: "public-read",
+//                     MetadataDirective: "COPY",
+//                     Key: `raw/${asset.Key}`,
+//                   }, (err) => {
+//                     if(err){
+//                       console.error(err);
+//                     }
+//                     innerCb();
+//                   }
+//                 );
+//               }
+//             );
+//           },
+//           err => {
+//             if (err) {
+//               cb(err);
+//             } else {
+//               if (!assetsList.IsTruncated) {
+//                 isMore = false;
+//               }
+//               cb();
+//             }
+//           }
+//         );
+//       }
+//     );
+//   },
+//   err => {
+//     if (err) throw err;
+//   }
+// );
